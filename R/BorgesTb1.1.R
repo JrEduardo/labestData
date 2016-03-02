80e68972ac7df0448fc83a9c943e10bc14cb5e8e
Alcides Conte Neto <neto_conte@hotmail.com>
2016-03-02 15:14:41 -0300

    Adicionado .R 1-4 cap1

---
+++ R/BorgesTb1.1.R
@@ -0,0 +1,23 @@
+#'  @name BorgesTb1.1
+#'  @title Fósforo Orgânico VS Fósforo Disponível
+#'  
+#'  @description O fósforo Orgânico foi adicionado antes de plantar-se o
+#'  milho. Após 38 dias, houve a colheita e mediu-se o fósforo 
+#'  disponível.
+#'  
+#'  @format Um data.frame de 9 linhas e 2 colunas.
+#'  
+#'  \describe{
+#'  
+#'      \item{\code{fo}}{ Fósforo orgânico adicionado ao solo. A unidade
+#'      de medida é partes por milhão (ppm).}
+#'      
+#'      \item{\code{fd}}{ Fósforo disponível após plantar-se o milho. 
+#'      A unidade de medida é partes por milhão (ppm).}
+#'  
+#'  }
+#'  
+#'  @keywords fósforo milho
+#'  
+#'  @source Borges, C. G., Demétrio, & Zocchi, S. S. (2011). 
+#'  Modelo de Regressão. Piracicaba, SP: USP.
