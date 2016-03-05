5ba012b3198abb104fc8608a0131daf4b52fe1dd
Altamiro Antonio Basiewics <altamiro@academia.leg.ufpr.br>
2016-03-05 15:53:24 -0300

    Tabela 1 da  página 68 concluida.

---
+++ R/BarrosDiasEg3.2.R
@@ -0,0 +1,18 @@
+#' @name BarrosDiasEg3.2
+#' @title Dados da produção de frutos de 43 cacaueiros.
+#' @description Número de frutos produzidos por cacaueiro.
+#' @format data.frame com 43 observações e 1 variável, em que
+#'
+#' \describe{
+#'
+#' \item{prod}{Número de frutos produzidos por cacaueiro.}
+#'
+#' }
+#' @keywords cacauiero produção
+#' @source Barros, W. S. &  Dias, L. A. S. (2009).
+#'     Biometria Experimental, UFV Viçosa, MG.
+#' @examples
+#'
+#' boxplot(BarrosDiasEg3.2)
+#'
+NULL
