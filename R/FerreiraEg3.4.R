0ce1b145df30f88d72d83097a82ea9f4a89fc97b
Eduardo Junior <edujrrib@gmail.com>
2016-03-07 00:19:23 -0300

    Adiciona dados do exemplo 3.4 do Ferreira

---
+++ R/FerreiraEg3.4.R
@@ -0,0 +1,34 @@
+#' @name FerreiraEg3.4
+#' @title Exame de sangue em pintores de carro
+#' @description Subconjunto de valores de um hemograma aplicado à
+#'     pintores de carros, apresentado por Royston (1983)
+#' @format \code{data.frame} com 20 observações e 2 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{conc}{Concentração de hemoglobina.}
+#'
+#' \item{linf}{Contagem de linfócitos}
+#'
+#' }
+#' @keywords hemograma
+#' @source Ferreira, D. F. (2011). Estatística Multivariada (2nd
+#'     ed.). Lavras, editora UFLA. (Exemplo 3.4)
+#'
+#' Royston, J. P. (1983). Some techniques for assessing multivariate
+#'     normality based on Shapiro-Wilk. London, Applied Statistics -
+#'     Journal of the Royal Statistical Society - Series C, v. 32, n. 2,
+#'     p. 121-133. (Table 2)
+#' 
+#' @examples
+#'
+#' # Normalidade univariada
+#' par(mfrow = c(1, 2))
+#' with(FerreiraEg3.4, {
+#'     qqnorm(conc, main = "Normal Q-Q Plot\nConc. Hemoglobina")
+#'     qqline(conc)
+#'     qqnorm(linf, main = "Normal Q-Q Plot\nCont. Linfócitos")
+#'     qqline(linf)
+#' })
+#'
+NULL
