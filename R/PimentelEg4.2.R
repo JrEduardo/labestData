b1a856f14e4116387e5a530e02466f054a073914
Bruno Geronymo <geronymobruno@hotmail.com>
2016-03-07 16:26:24 -0300

    Concerta erro ortográfico de PimentelEg4.2.R

--- R/PimentelEg4.2.R
+++ R/PimentelEg4.2.R
@@ -16,7 +16,7 @@
 #'
 #' }
 #' @keywords DIC rações
-#' @source Pimentel-Gomes, F. (2009). Curso de Estatístitica
+#' @source Pimentel-Gomes, F. (2009). Curso de Estatística
 #'     Experimental (15th ed.). Piracicaba: FEALQ.
 #' @examples
 #'
