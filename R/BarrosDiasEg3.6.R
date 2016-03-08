546d40145bcd77586fb48ef30f7f6f268f038f2d
Altamiro Antonio Basiewics <altamiro@academia.leg.ufpr.br>
2016-03-08 00:40:41 -0300

    tabela 3.6 capitulo 3 Biometria Experimental pronta

---
+++ R/BarrosDiasEg3.6.R
@@ -0,0 +1,22 @@
+#' @name BarrosDiasEg3.6
+#' @title alturas de 100 plantas de milho.
+#' @description amostras das alturas de 100 plantas de milho
+#'     tomadas aleatoriamente.
+#' @format data.frame com 100 observações e 1 variável, em que
+#'
+#' \describe{
+#'
+#' \item{altura}{altura das plantas de milho}
+#'
+#' }
+#' @keywords altura milho
+#' @source L.A.S. Dias W.S. Barros,Biometria Experimental,
+#'      Viçosa MG, 2009.
+#'    
+#' @examples
+#'
+#' library(lattice)
+#'
+#' boxplot(BarrosDiasEg3.6)
+#'
+NULL
