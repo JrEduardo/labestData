b1cf422c2112830b324476b64a1d35207c9a0323
Jhenifer <jhenicaet@gmail.com>
2016-03-09 16:03:20 -0300

    Verifica documentação e adiciona tabela RamalhoEg4.3 ao pacote.

---
+++ R/RamalhoEg4.3.R
@@ -0,0 +1,30 @@
+#' @name RamalhoEg4.3
+#' @title Porcentagem de absorção de água de feijão.
+#' @description Experimento conduzido na Universidade Federal de Lavras, 
+#'     envolvendo a porcentagem de absorção de água de 10 linhares de 
+#'     feijão com 3 repetições. O delineamento é inteiramente ao acaso.
+#' @format data.frame com 30 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{linh}{Número inteiro que representa a linhagem do feijão.}
+#'
+#' \item{rep}{Número inteiro que indica as repetições do experimento.}
+#'
+#' \item{abs}{Porcentagem de absorção de água.}
+#'
+#' }
+#' @keywords água feijão
+#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005). 
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.). 
+#'     Lavras: UFLA. (pg 56)
+#' @examples
+#'
+#' library(lattice)
+#'
+#' data(RamalhoEg4.3)
+#' 
+#' aggregate(abs ~ linh,  data = RamalhoEg4.3, 
+#'           FUN = function(x) { c(mean = mean(x), var = var(x)) })
+#'
+NULL
