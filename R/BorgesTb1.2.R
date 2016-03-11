eb75ca8c0befc71fe114cbafc1af06e44c8d54ce
Eduardo Junior <edujrrib@gmail.com>
2016-03-11 02:11:13 -0300

    Corrige documentação e simplifica exemplo na tabela 1.2

--- R/BorgesTb1.2.R
+++ R/BorgesTb1.2.R
@@ -1,20 +1,22 @@
 #' @name BorgesTb1.2
 #' @title Irrigação em Batata
 #' 
-#' @description Foram medidas as lâminas de água a diferentes 
-#'  disntâncias do aspersor. O objetivo foi mensurar a produtividade, 
-#'  já que, no tipo de solo utilizado no experimento (solo argiloso),
-#'  espera-se que o excesso de água diminua a produtividade.
+#' @description Este experimento refere-se a irrigação em batata
+#'     plantada em terra roxa estruturada. Foram medidas as lâminas de
+#'     água a diferentes distâncias do aspersor. O objetivo foi mensurar
+#'     a produtividade, já que, no tipo de solo utilizado no experimento
+#'     (solo argiloso), espera-se que o excesso de água diminua a
+#'     produtividade.
 #' 
-#' @format Um \code{data.frame} de 12 linhas e 2 colunas.
+#' @format Um \code{data.frame} com 12 linhas e 2 colunas.
 #' 
 #' \describe{
 #' 
-#'     \item{\code{lamina}}{ Espessura da lâmina de água medida em 
-#'     milímetros (mm). }
+#'     \item{\code{lamina}}{Espessura da lâmina de água medida em
+#'     milímetros (mm).}
 #'     
-#'     \item{\code{prod}}{ Produtividade medida em toneladas de batatas
-#'      por hectare (t/ha). }
+#'     \item{\code{prod}}{Produtividade medida em toneladas de batatas
+#'      por hectare (t/ha).}
 #' 
 #' }
 #' 
@@ -25,27 +27,13 @@
 #' 
 #' @examples
 #' 
-#' ## Carregando pacotes necessários -----------------------------------
-#' 
 #' library(lattice)
-#' 
-#' ## Carregando o banco de dados --------------------------------------
 #' 
 #' data(BorgesTb1.2)
 #' 
-#' ## Plotando o gráfico -----------------------------------------------
-#' 
 #' xyplot(prod ~ lamina, data = BorgesTb1.2,
-#'     main = "Produção VS Lâmina de Água",
-#'     xlab = "Lâmina",
-#'     ylab = "Produção",
-#'     panel = function(x, y) {
-#'         panel.xyplot(x, y)
-#'         panel.abline(lm(y ~ x), col = 3)
-#'     })
-#'     
-#' ## Verificando Significância ----------------------------------------
-#' 
-#' summary(lm(prod ~ lamina, data = BorgesTb1.2))
-#'
+#'        main = "Produção VS Lâmina de Água",
+#'        xlab = "Lâmina de água (mm)",
+#'        ylab = "Produção (t/ha)",
+#'        type = c("p", "r"), col.line = 3)
 NULL
