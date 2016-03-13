d7ac3bbaa60a045b56780376a808799909f2d9b4
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 11:53:21 -0300

    Corrige documentação.

--- R/RamalhoTb3.6.R
+++ R/RamalhoTb3.6.R
@@ -1,28 +1,28 @@
 #' @name RamalhoTb3.6
-#' @title Largura dos ascos do fungo colletrotrichum lindemuthianum.
-#' @description Experimento referente a largura dos ascos de três 
-#'     isolados do fungo Colletotrichum lindemuthianum.
+#' @title Largura de ascos \emph{Colletrotrichum lindemuthianum}
+#' @description Experimento referente a largura dos ascos de três
+#'     isolados do fungo \emph{Colletotrichum lindemuthianum}.
 #' @format data.frame com 90 observações e 3 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{amostra}{Experimento com 30 observações em cada bloco.}
-#' 
-#' \item{isol}{Blocos dos isolados do fungo (A, B e C).}
+#' \item{amostra}{Número identificador da amostra.}
 #'
-#' \item{larg}{Largura dos ascos dos isolados.}
+#' \item{isol}{Isolados do fungo (A, B e C).}
+#'
+#' \item{larg}{Largura dos ascos dos isolados (TODO unidade de medida).}
 #'
 #' }
-#' @keywords largura fungo
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005). 
-#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.). 
+#' @keyword NULL
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
 #'     Lavras: UFLA. (pg 48)
 #'
 #' @examples
 #'
 #' data(RamalhoTb3.6)
-#' 
-#' aggregate(larg ~ isol,  data = RamalhoTb3.6, 
+#'
+#' aggregate(larg ~ isol,  data = RamalhoTb3.6,
 #'           FUN = function(x) { c(mean = mean(x), var = var(x)) })
-#' 
+#'
 NULL
