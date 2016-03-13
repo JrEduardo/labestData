0bd95c2049dec45262a8e014ef36523e86b3dafb
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 17:16:51 -0300

    Revisão da documentação.

--- R/MingotiTb3.1.R
+++ R/MingotiTb3.1.R
@@ -1,34 +1,33 @@
 #' @name MingotiTb3.1
-#' @title Dados relativos às empresas
-#'
-#' @description Dados de 12 empresas sobre ganhos (bruto e líquido) e patrimônio acumulado num determinado período.
-#' 
+#' @title Receitas e patrimônio de empresas
+#' @description Dados de 12 empresas sobre ganhos (bruto e líquido) e
+#'     patrimônio acumulado num determinado período.
 #' @format Um \code{data.frame} com 12 observações e 4 variáveis, em que
-#' 
+#'
 #' \describe{
 #'
-#' \item{emp}{Identificação da empresa.}
-#' 
-#' \item{gbt}{Ganho bruto da empresa (em unidades monetárias).}
-#' 
-#' \item{glq}{Ganho líquido da empresa (em unidades monetárias).}
-#' 
-#' \item{patr}{Patrimônio acumulado (unidades monetárias).}
-#' }
+#' \item{\code{emp}}{Identificação da empresa.}
 #'
-#' @keywords empresas ganhos
-#' 
-#' @source MINGOTI, S.A. (2005). Análise de dados através de métodos de 
-#'      estatística multivariada - uma abordagem aplicada. 
-#'      Belo Horizonte, MG: Editora UFMG. (pg 42).
+#' \item{\code{gbt}}{Ganho bruto da empresa (em unidades monetárias).}
+#'
+#' \item{\code{glq}}{Ganho líquido da empresa (em unidades monetárias).}
+#'
+#' \item{\code{patr}}{Patrimônio acumulado (unidades monetárias).}
+#'
+#' }
+#' @keywords TODO
+#' @source Mingoti, S. A. (2005). Análise de dados através de métodos de
+#'     estatística multivariada: uma abordagem aplicada.  Belo
+#'     Horizonte, MG: Editora UFMG. (pg 42).
 #' @examples
 #'
-#' data(MingotiTb3.1) 
-#' 
 #' library(car)
-#' 
-#' scatterplotMatrix(~ emp + gbt + glq + patr, data = MingotiTb3.1, 
-#'                 main="Dispersão das variáveis")
-#' 
+#'
+#' data(MingotiTb3.1)
+#'
+#' scatterplotMatrix(~gbt + glq + patr,
+#'                   data = MingotiTb3.1,
+#'                   main = "Dispersão das variáveis")
+#'
 #'
 NULL
--- R/MingotiTb3.5.R
+++ R/MingotiTb3.5.R
@@ -1,38 +1,42 @@
 #' @name MingotiTb3.5
-#' @title Dados relativos as marcas de coxinha de galinha
-#'
-#' @description Dados de 8 marcas de coxinha de galinha, avaliados em 4 
-#'          atributos (sabor, aroma, qualidade da massa e qualidade do recheio) 
-#'          avaliados por 5 julgadores numa escala de 1 a 5, quanto maior a nota melhor a classificação do atributo.
-#'          Os dados de entrada da tabela referem-se à média das notas dos 5 julgadores, para cada marca e cada atributo.
-#' 
+#' @title Impressões sensoriais de marcas de coxinha de galinha
+#' @description Dados de 8 marcas de coxinha de galinha, avaliados em 4
+#'     atributos (sabor, aroma, qualidade da massa e qualidade do
+#'     recheio) avaliados por 5 julgadores numa escala de 1 a 5, quanto
+#'     maior a nota melhor a classificação do atributo.  Os dados de
+#'     entrada da tabela referem-se à média das notas dos 5 julgadores,
+#'     para cada marca e cada atributo.
 #' @format Um \code{data.frame} com 8 observações e 5 variáveis, em que
-#' 
+#'
 #' \describe{
 #'
-#' \item{marca}{Identificação da marca.}
-#' 
-#' \item{sabor}{Média das notas dos juízes para o sabor da coxinha de galinha.}
-#' 
-#' \item{aroma}{Média das notas dos juízes para o aroma da coxinha de galinha.}
-#' 
-#' \item{massa}{Média das notas dos juízes para a qualidade da massa.}
-#' 
-#' \item{recheio}{Média das notas dos juízes para a qualidade do recheio.}
-#' }
+#' \item{\code{marca}}{Identificação da marca.}
 #'
-#' @keywords coxinha qualidade
-#' 
-#' @source MINGOTI, S.A. (2005). Análise de dados através de métodos de 
-#'      estatística multivariada - uma abordagem aplicada. 
-#'      Belo Horizonte, MG: Editora UFMG. (pg 42).
+#' \item{\code{sabor}}{Média das notas dos juízes para o sabor da
+#'     coxinha de galinha.}
+#'
+#' \item{\code{aroma}}{Média das notas dos juízes para o aroma da
+#'     coxinha de galinha.}
+#'
+#' \item{\code{massa}}{Média das notas dos juízes para a qualidade da
+#'     massa.}
+#'
+#' \item{\code{recheio}}{Média das notas dos juízes para a qualidade do
+#'     recheio.}
+#'
+#' }
+#' @keywords sensorial
+#' @source Mingoti, S. A. (2005). Análise de dados através de métodos de
+#'     estatística multivariada: uma abordagem aplicada.  Belo
+#'     Horizonte, MG: Editora UFMG. (pg 42).
 #' @examples
 #'
-#' data(MingotiTb3.5) 
-#' 
+#' data(MingotiTb3.5)
+#'
 #' library(car)
-#' 
-#' scatterplotMatrix(~ marca + sabor + aroma + massa + recheio, data = MingotiTb3.5, 
-#'                     main="Dispersão das variáveis")
+#'
+#' scatterplotMatrix(~sabor + aroma + massa + recheio,
+#'                   data = MingotiTb3.5,
+#'                   main = "Matriz de dispersão")
 #'
 NULL
