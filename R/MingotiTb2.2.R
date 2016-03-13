498c5cec6c6a495f1c58ac9b6bb0855805c8cd10
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 16:36:51 -0300

    Corrige documentação, edita data.frame.

--- R/MingotiTb2.2.R
+++ R/MingotiTb2.2.R
@@ -1,33 +1,30 @@
 #' @name MingotiTb2.2
 #' @title Notas de estudantes em três provas
-#' 
-#' @description Notas obtidas por 19 estudantes em três provas de 
-#'      uma determinada disciplina.
-#'          
-#' @format data.frame com 19 observações e 4 variáveis, em que
+#' @description Notas obtidas em uma turma de estudantes em três provas
+#'     de uma determinada disciplina.
+#' @format Um \code{data.frame} com 19 observações e 4 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{est}{Identificação do estudante.}
+#' \item{\code{est}}{Identificação do estudante.}
 #'
-#' \item{p1}{Nota obtida na primeira prova variando de 0 a 25.}
+#' \item{\code{p1}}{Nota obtida na primeira prova variando de 0 a 25.}
 #'
-#' \item{p2}{Nota obtida na segunda prova variando de 0 a 25.}
+#' \item{\code{p2}}{Nota obtida na segunda prova variando de 0 a 25.}
 #'
-#' \item{p3}{Nota obtida na terceira prova variando de 0 a 25.}
+#' \item{\code{p3}}{Nota obtida na terceira prova variando de 0 a 25.}
+#'
 #' }
-#' @keywords Nota estudante
-#' 
-#' @source MINGOTI, S.A. (2005). Análise de dados através de métodos de 
-#'      estatística multivariada - uma abordagem aplicada. 
-#'      Belo Horizonte, MG: Editora UFMG. (pg 52).
-#'      
+#' @keywords TODO
+#' @source Mingoti, S. A. (2005). Análise de dados através de métodos de
+#'     estatística multivariada: uma abordagem aplicada.  Belo
+#'     Horizonte, MG: Editora UFMG. (pg 52).
 #' @examples
-#' 
+#'
 #' data(MingotiTb2.2)
-#' 
-#' pairs(~ est + p1 + p2 + p3, 
-#'       data = MingotiTb2.2, 
-#'       main="Dispersão das três variáveis")
+#'
+#' pairs(~ p1 + p2 + p3,
+#'       data = MingotiTb2.2,
+#'       main = "Matriz de dispersão das 4 notas")
 #'
 NULL
--- R/MingotiTb8.1.R
+++ R/MingotiTb8.1.R
@@ -1,39 +1,38 @@
 #' @name MingotiTb8.1
-#' @title Renda e o número de filhos 
-#' 
-#' @description Dados relativos a 257 indivíduos classificados de 
-#'     acordo pela renda e número de filhos.
-#'       
-#' @format data.frame com 3 observações e 5 variáveis, em que
+#' @title Renda e o número de filhos
+#' @description Dados relativos a 257 indivíduos classificados de acordo
+#'     com a renda e número de filhos.
+#' @format Um \code{data.frame} com 3 observações e 5 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{renda}{Renda, medida em 3 categorias (menos de 2000, 2000 a 
-#'      5000 e 5000 ou mais), em unidades monetárias.}
+#' \item{\code{renda}}{Renda, medida em 3 categorias (menos de 2000,
+#'      2000 a 5000 e 5000 ou mais), em unidades monetárias.}
 #'
-#' \item{f0}{Indivíduos que não tem filhos.}
+#' \item{\code{i0f}}{Indivíduos que não tem filhos (igual a zero
+#'     filhos).}
 #'
-#' \item{f1}{Indivíduos que tem 1 filho.}
+#' \item{\code{i1f}}{Indivíduos que tem 1 filho (igual a um filho).}
 #'
-#' \item{f2}{Indivíduos que tem 2 filhos.}
-#' 
-#' \item{fmaior}{Indivíduos que tem mais de 2 filhos.}
+#' \item{\code{i2f}}{Indivíduos que tem 2 filhos (igual a dois filhos).}
+#'
+#' \item{\code{m2f}}{Indivíduos que tem mais de 2 filhos (mais que dois
+#'     filhos).}
+#'
 #' }
-#' @keywords Renda e número de filhos
-#' 
-#' @source MINGOTI, S.A. (2005). Análise de dados através de métodos de 
-#'     estatística multivariada - uma abordagem aplicada. 
-#'     Belo Horizonte, MG: Editora UFMG. (pg 258).
-#'     
+#' @keywords TODO
+#' @source Mingoti, S. A. (2005). Análise de dados através de métodos de
+#'     estatística multivariada: uma abordagem aplicada.  Belo
+#'     Horizonte, MG: Editora UFMG. (pg 258).
 #' @examples
-#' 
-#'data(MingotiTb8.1)
-#' 
-#'library(lattice)
 #'
-#' xyplot(f0 + f1 + f2 + fmaior ~ renda, 
+#' data(MingotiTb8.1)
+#'
+#' library(lattice)
+#'
+#' xyplot(i0f + i1f + i2f + m2f ~ renda,
 #'        data = MingotiTb8.1,
-#'        ylab="Número de indivíduos",
-#'        auto.key = TRUE)    
+#'        ylab = "Número de indivíduos",
+#'        auto.key = TRUE)
 #'
 NULL
