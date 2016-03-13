8f19242f673a7e1bdcc6488a2eb1604e3ea91dfb
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 18:50:59 -0300

    Correções na documentação.

--- R/PimentelTb5.3.1.R
+++ R/PimentelTb5.3.1.R
@@ -1,25 +1,27 @@
 #' @name PimentelTb5.3.1
 #' @title Teor de colesterol no sangue
 #' @description Pesquisa sobre o efeito do óleo de milho no teor de
-#'     colesterol do sangue, realizada em sete pacientes tomados
-#'     como blocos, cujos dados foram obtidos pelo médico Dr. Ben 
-#'     Hur C. Paiva.
-#' @format data.frame com 14 observações e 3 variáveis, em que
+#'     colesterol do sangue, realizada em sete pacientes tomados como
+#'     blocos, cujos dados foram obtidos pelo médico Dr. Ben Hur
+#'     C. Paiva.
+#' @format Um \code{data.frame} com 14 observações e 3 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{periodo}{Fator de 2 níveis qualitativos, usado para comparar o
-#'     teor de colesterol no sangue antes e depois do uso do óleo de 
-#'     milho.}
+#' \item{\code{periodo}}{Fator de 2 níveis qualitativos, usado para
+#'     comparar o teor de colesterol no sangue antes e depois do uso do
+#'     óleo de milho.}
 #'
-#' \item{paciente}{Variável utilizada para identificar os 7 pacientes.}
+#' \item{\code{paciente}}{Variável utilizada para identificar os 7
+#'     pacientes.}
 #'
-#' \item{colesterol}{Teor de colesterol no sangue em mg por 100g.}
+#' \item{\code{colesterol}}{Teor de colesterol no sangue em mg por
+#'     100g.}
 #'
 #' }
-#' @keywords DBC colesterol
-#' @source Pimentel-Gomes, F. (2009). Curso de Estatística
-#'     Experimental (15th ed.). Piracicaba: FEALQ.
+#' @keywords DBC
+#' @source Pimentel-Gomes, F. (2009). Curso de Estatística Experimental
+#'     (15th ed.). Piracicaba, SP: FEALQ.
 #' @examples
 #'
 #' library(lattice)
