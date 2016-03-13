18f4e549ef37213d176914e9db6a79f400a34dfa
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 11:14:23 -0300

    Revfisa documentação, reescreve txt e rda.

--- R/RamalhoEx3.1.R
+++ R/RamalhoEx3.1.R
@@ -1,33 +1,32 @@
 #' @name RamalhoEx3.1
-#' @title Número de plantes de milho após inoculação.
-#' @description Experimento referente ao número de plantas de milho, 
-#'     por parcela, após a inoculação com Diploidia. Experimento para 
+#' @title Número de plantas de milho após inoculação
+#' @description Experimento referente ao número de plantas de milho,
+#'     por parcela, após a inoculação com Diploidia. Experimento para
 #'     avaliação de fungicidas para tratamento de sementes.
 #' @format data.frame com 20 observações e 4 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{repet}{Repetições de inoculação do fungicida.}
-#' 
-#' \item{fugic}{Tipo deFungicida.}
+#' \item{fugic}{Tipo de fungicida aplicado.}
 #'
-#' \item{plant}{Número de plantes de milho.}
-#' 
-#' \item{contr}{Controle.}
-#' 
+#' \item{rept}{Repetições de inoculação do fungicida.}
+#'
+#' \item{plant}{Número de plantas de milho observadas.}
+#'
 #' }
-#' @keywords milho inoculação
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005). 
-#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.). 
+#' @keywords contagem
+#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
 #'     Lavras: UFLA. (pg 50)
 #'
 #' @examples
+#'
 #' library(lattice)
-#' 
+#'
 #' data(RamalhoEx3.1)
-#' 
-#' xyplot(jitter(plant) ~ fungic, groups = repet, data = RamalhoEx3.1, 
-#'        auto.key = TRUE, 
+#'
+#' xyplot(jitter(plant) ~ fungic, data = RamalhoEx3.1,
+#'        groups = repet, auto.key = TRUE,
 #'        xlab = "Fungicida", ylab = "Número de plantas")
-#' 
+#'
 NULL
