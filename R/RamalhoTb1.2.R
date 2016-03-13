95c061a1fbf916883f595dd852bcd21d86e0a1f6
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 11:22:21 -0300

    Revisa documentação, aumenta Exemplos.

--- R/RamalhoTb1.2.R
+++ R/RamalhoTb1.2.R
@@ -1,27 +1,38 @@
 #' @name RamalhoTb1.2
 #' @title Produtividade Média de Espiga
-#' @description Experimento da produtividade média de espigas de milho 
+#' @description Experimento da produtividade média de espigas de milho
 #'     de diversas cultivares obtidas por Ferreira et al (1995).
 #' @format data.frame com 28 observações e 2 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{varied}{Fator de níveis nominais das variedades de milho. }
+#' \item{varied}{Fator de níveis nominais das variedades de milho.}
 #'
-#' \item{t.ha}{Produtividade média de espiga (t/ha).}
+#' \item{prod}{Produtividade média de espiga (ton ha\eqn{^{-1}}).}
 #'
 #' }
-#' @keywords milho produtividade
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005). 
-#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.). 
+#' @keywords ASS
+#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
 #'     Lavras: UFLA. (pg 15)
 #'
 #' @examples
 #'
-#' ht <- hist(RamalhoTb1.2$t.ha, xlab = "t.ha", ylab = "Frequência", 
-#'           main = "", breaks = c(2.5, 4.25, 6, 7.75, 9.5), axes = FALSE, 
-#'           labels = TRUE)
-#' axis(1, at = c(2.5, 4.25, 6, 7.75, 9.5))
-#' axis(2)
-#' 
+#' bk <- c(2.5, 4.25, 6, 7.75, 9.5)
+#' ht <- hist(RamalhoTb1.2$prod,
+#'            xlab = expression("Produtividade"~(ton~ha^{-1})),
+#'            ylab = "Frequência absoluta",
+#'            main = NULL,
+#'            breaks = bk,
+#'            axes = FALSE,
+#'            labels = TRUE)
+#' axis(side = 1, at = bk)
+#' axis(side = 2)
+#' rug(RamalhoTb1.2$prod)
+#'
+#' plot(ecdf(RamalhoTb1.2$prod),
+#'      xlab = expression("Produtividade"~(ton~ha^{-1})),
+#'      ylab = "Frequência relativa acumulada",
+#'      main = NULL)
+#'
 NULL
