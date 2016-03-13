a1ac5ce646d921aaa9506df3bcdf00af4632185b
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 13:23:35 -0300

    Corrige documentação e renomeia colunas.

--- R/ManlyTb1.1.R
+++ R/ManlyTb1.1.R
@@ -1,37 +1,33 @@
 #' @name ManlyTb1.1
 #' @title Pardais sobreviventes da tempestade
-#'
-#' @description Estudo em 1898, para a teoria da evolução de Darwin com 
-#'   pardais moribundos, com total de 49 dados.
-#'
+#' @description Estudo em 1898, para a teoria da evolução de Darwin com
+#'     pardais moribundos, com total de 49 dados.
 #' @format Um \code{data.frame} com 49 registros e 6 variáveis.
-#' 
-#' \itemize{
 #'
-#' \item \code{ct} Comprimento total do pardal em (mm).
-#' 
-#' \item \code{ea} Extensão alar em (mm).
-#' 
-#' \item \code{cbc} Comprimento do bico e cabeça em (mm).
+#' \describe{
 #'
-#' \item \code{cdu} Comprimento do úmero em (mm).
-#' 
-#' \item \code{cqe} Comprimento da quilhado esterno em (mm).
-#' 
-#' \item \code{sobre} Pardal Sobrevivel. S = Sim e N = Não 
+#' \item{\code{ct}}{Comprimento total do pardal (mm).}
+#'
+#' \item{\code{ea}}{Extensão alar (mm).}
+#'
+#' \item{\code{cbc}}{Comprimento do bico e cabeça (mm).}
+#'
+#' \item{\code{cdu}}{Comprimento do úmero (mm).}
+#'
+#' \item{\code{cqe}}{Comprimento da quilhado esterno (mm).}
+#'
+#' \item{\code{sobrev}}{Se o animal sobreviveu: S = Sim e N = Não.}
+#'
 #' }
 #'
-#' @docType data.frame
-#' @keywords Dados Multivariados
-#' @usage data(ManlyTb1.1)
-#' @source Manly, B. J. F. (2005). Métodos Estatísticos Multivariados- uma introdução.
-#'      Porto Alegre, RS: ARTMED (pg 14 e 15)
+#' @keywords TODO
+#' @source Manly, B. J. F. (2005). Métodos Estatísticos Multivariados:
+#'     uma introdução. Porto Alegre, RS: ARTMED (pg 14 e 15)
 #' @examples
 #'
 #' data(ManlyTb1.1)
-#' 
-#' pairs(~ ct + ea + cbc + cdu + cqe + sobre,
-#'       data = ManlyTb1.1, 
-#'       main="Gráfico de dispersão dos pardais")
+#'
+#' pairs(~ ct + ea + cbc + cdu + cqe + sobrev,
+#'       data = ManlyTb1.1,
+#'       main = "Gráfico de dispersão das variáveis nos pardais")
 NULL
-
