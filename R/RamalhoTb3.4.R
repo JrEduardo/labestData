bbccd2956b7359855af86f78a73f4903c3568050
Walmes Zeviani <walmes@ufpr.br>
2016-03-13 11:44:31 -0300

    Revisa documentação, renomeia coluna.

--- R/RamalhoTb3.4.R
+++ R/RamalhoTb3.4.R
@@ -1,36 +1,37 @@
 #' @name RamalhoTb3.4
-#' @title Médias de incidênica de Colletotrichum no feijoeiro.
-#' @description Neste experimento foram avaliados 16 tratamentos com 3 
-#'     observações em blocos casualizados referentes a incidência de 
-#'     Colletotrichum no feijoeiro, as notas foram dadas por 3 
+#' @title Incidênica de \emph{Colletotrichum} no feijoeiro
+#' @description Neste experimento foram avaliados 16 tratamentos com 3
+#'     observações em blocos casualizados referentes a incidência de
+#'     Colletotrichum no feijoeiro, as notas foram dadas por 3
 #'     avaliadores.
 #' @format data.frame com 48 observações e 4 variáveis, em que
 #'
 #' \describe{
 #'
 #' \item{trat}{Tratamento.}
-#' 
-#' \item{blocos}{Blocos onde são aplicados os tratamentos.}
+#'
+#' \item{bloc}{Blocos onde são aplicados os tratamentos.}
 #'
 #' \item{cult}{Cultivar de feijão.}
-#' 
-#' \item{nota}{Média dos avaliadores.}
+#'
+#' \item{nota}{Variável reposta que é a nota atribuída pelos avaliadores
+#'     para a incidência e \emph{Colletotrichum}.}
 #'
 #' }
-#' @keywords média fejoeiro
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005). 
-#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.). 
+#' @keywords DBC
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
 #'     Lavras: UFLA. (pg 45)
 #'
 #' @examples
 #'
 #' library(lattice)
-#' 
+#'
 #' data(RamalhoTb3.1)
-#' 
-#' xyplot(nota ~ jitter(cult), groups = bloco, data = RamalhoTb3.4, 
-#'        auto.key = TRUE, 
+#'
+#' xyplot(nota ~ cult, data = RamalhoTb3.4,
+#'        groups = bloco, auto.key = TRUE, jitter.x = TRUE,
 #'        xlab = "Cultivares",
 #'        ylab = "Notas")
-#' 
+#'
 NULL
