e3cf6ca38c6986762065053cc83a6b39198596cf
Eduardo Junior <edujrrib@gmail.com>
2016-03-13 21:24:59 -0300

    Revisa documentação simplificando exemplo e padronizando referência

--- R/CharnetAp.D.R
+++ R/CharnetAp.D.R
@@ -24,35 +24,30 @@
 #'
 #' }
 #'
-#' @keywords bailarinas regressão
+#' @keywords TODO
 #'
 #' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino, 
 #' H. (2008). Análise de modelos de regressão linear com aplicações (2nd
-#' ed., p. 356). Editora Unicamp (Apêndice D, página 325)
+#' ed., p. 356). SP: Editora Unicamp (Apêndice D, página 325)
 #'
 #' @examples
 #'
 #' data(CharnetAp.D)
 #' 
-#' library("ggplot2")
+#' library(ggplot2)
 #'
-#' bailarinas <- qplot(peso, altura,
-#'                     data = CharnetAp.D,
-#'                     color = idade,
-#'                     xlab = "Peso",
-#'                     ylab = "Altura",
-#'                     main = "Relação Peso e Altura por 
-#'                             Idade de Jovens Bailarinas")
+#' qplot(peso, altura,
+#'       data = CharnetAp.D,
+#'       color = idade,
+#'       xlab = "Peso",
+#'       ylab = "Altura",
+#'       main = "Relação Peso e Altura por Idade de Jovens Bailarinas")
 #'
-#' bailarinas
-#'
-#'
-#' bailarinas2 <- qplot(ped, pee, 
+#' bailarinas <- qplot(ped, pee,
 #'                      data = CharnetAp.D,
 #'                      xlab = "Angulação do pé direito",
 #'                      ylab = "Angulação do pé esquerdo",
 #'                      main = "Diferença na Angulação Entre os Pés")
 #'
-#' bailarinas2 + geom_abline(intercept = 0, slope = 1)
-
+#' bailarinas + geom_abline(intercept = 0, slope = 1)
 NULL
