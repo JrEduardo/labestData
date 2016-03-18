23096b3d8fb8518b086344970e304a0ffe0c60eb
Bruno Geronymo <geronymobruno@hotmail.com>
2016-03-18 18:23:38 -0300

    Tabela 4 (PimentelPg91) concluída.

---
+++ R/PimentelPg91.R
@@ -0,0 +1,33 @@
+#' @name PimentelPg91
+#' @title Dados de produção de milho
+#' @description Experimento em blocos casualizados realizado pelo
+#'     Engenheiro Agrônomo Duvilio Ometto, com 2 tratamentos (aradura
+#'     profunda e aradura superficial) e 6 blocos com 2 repetições cada.
+#' @format Um \code{data.frame} com 24 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{aradura}}{Fator de 2 níveis qualitativos, que são os
+#'     diferentes tipos de aradura.}
+#'
+#' \item{\code{bloco}}{Fator de 6 níveis qualitativos, usado para
+#'     controle local.}
+#'     
+#' \item{\code{prod}}{Produção de milho, em kg por parcela de
+#'     200\eqn{m^{2}}.}
+#'
+#' }
+#' @keywords DBC
+#' @source Pimentel-Gomes, F. (2009). Curso de Estatística Experimental
+#'     (15th ed.). Piracicaba, SP: FEALQ.
+#' @examples
+#'
+#' library(lattice)
+#'
+#' bwplot(prod ~ aradura,
+#'        groups = bloco,
+#'        data = PimentelPg91,
+#'        xlab = "Aradura",
+#'        ylab = "Produção")
+#'
+NULL
