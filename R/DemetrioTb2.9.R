54ba6c9af0212e60704fced502c1f6ea326a47c8
Alcides Conte Neto <neto_conte@hotmail.com>
2016-03-19 12:43:42 -0300

    Arrumando nome para Tb2.9

---
+++ R/DemetrioTb2.9.R
@@ -0,0 +1,38 @@
+#' @name DemetrioTb2.9
+#' @title Pares de Valores
+#' 
+#' @description Os dados estão presentes no exercício com o objetivo de
+#'     estimar os parâmetros, realizar a ANOVA, entre outros.
+#' 
+#' @format Um \code{data.frame} de 10 linhas e 2 colunas.
+#' 
+#' \describe{
+#' 
+#'     \item{\code{x}}{Sem interpretação.}
+#'     
+#'     \item{\code{y}}{Sem interpretação.}
+#'     
+#' }
+#' 
+#' @keywords TODO
+#' 
+#' @source Demétrio, C. G. B., & Zocchi, S. S. (2011). Modelos de
+#'     Regressão. Piracicaba: ESALQ. (Tabela 2.9 pág. 63)
+#' 
+#' @examples 
+#' 
+#' data(DemetrioTb2.9)
+#' 
+#' library(lattice)
+#' 
+#' xyplot(y ~ x, data = DemetrioTb2.9,
+#'      main = "x vs y",
+#'      xlab = "x",
+#'      ylab = "y",
+#'      type = c("p", "r"), col.line = 3)
+#'      
+#' model <- lm(y ~ x)
+#' summary(model)
+#' anova(model)
+#' 
+NULL
