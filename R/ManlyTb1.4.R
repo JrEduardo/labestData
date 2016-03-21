331cf8528f894ed0de007608879001e05e0b577a
gabriel sartori <gabrielsartori2008@hotmail.com>
2016-03-21 15:17:46 -0300

    Alteração na tabela 1.4, segundo as recomendações do Cesar

--- R/ManlyTb1.4.R
+++ R/ManlyTb1.4.R
@@ -1,14 +1,14 @@
 #' @name ManlyTb1.4
 #' @title Cães pré-históricos da Tailândia
 #'
-#' @description Estudo em ancestrais de cães pré-históricos da Tailândia, 
-#' através de medições da mandíbula com espécies disponíveis.
+#' @description Estudo em ancestrais de cães da Tailândia, 
+#' através de medições da mandíbula.
 #'
-#' @format Um \code{data.frame} com 7 grupos caninos e 6 variáveis.
+#' @format Um \code{data.frame} com 7 grupo canino e 6 variáveis.
 #' 
 #' \describe{
 #'
-#' \item {\code{grupo}} {Grupo canino}
+#' \item {\code{grup}} {Grupo canino}
 #' 
 #' \item {\code{largm}} {Largura da mandíbula (mm)}
 #' 
@@ -18,9 +18,9 @@
 #' 
 #' \item {\code{largpm}} {Largura do primeiro molar (mm).}
 #' 
-#' \item {\code{compptm}} {Comprimento do primeiro ao terceiro molar (mm).}
+#' \item {\code{comppt}} {Comprimento do primeiro ao terceiro molar (mm).}
 #' 
-#'  \item {\code{comppqm}} {Comprimento do primeiro ao quarto molar (mm).}
+#'  \item {\code{comppq}} {Comprimento do primeiro ao quarto molar (mm).}
 #' }
 #'
 #' @keywords Todo
@@ -30,9 +30,7 @@
 #'
 #' data(ManlyTb1.4)
 #' 
-#' pairs(~ grupo + largm + altm + comppm + largpm + comppt + comppq,
+#' pairs(~largm + altm + comppm + largpm + comppt + comppq,
 #'       data = ManlyTb1.4, 
-#'       main="Gráfico de dispersão dos cães")
+#'       main="Gráfico de dispersão para as medições da mandíbula")
 NULL
-
-
