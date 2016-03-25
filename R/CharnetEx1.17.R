630cae1a0782f137c7946d2203fc6cd291bb4780
Eduardo Junior <edujrrib@gmail.com>
2016-03-25 20:50:49 -0300

    Modifica nome de variável, corrige e complementa documentação
    O dataset refere-se a dois conjuntos de dados, portanto a coluna
    `amos`, foi renomeada para `conj` representando a qual conjunto a
    observação pertence. Além disso a documentação foi ampliada.

--- R/CharnetEx1.17.R
+++ R/CharnetEx1.17.R
@@ -1,22 +1,28 @@
 #' @name CharnetEx1.17
 #' 
-#' @title Dois conjuntos de valores X e Y
+#' @title Dados Genéricos para Regressão Linear Simples
 #' 
-#' @description Dois conjuntos de valores X e Y
+#' @description Dois conjuntos de valores X e Y utilizados para
+#'     para exemplificação de regressão linear simples.
 #'
 #' @format Um \code{data.frame} com 3 colunas e 45 linhas.
 #'
 #' \describe{
 #'
-#'     \item{\code{amos}}{Conjunto de valores da amostra}
+#'     \item{\code{conj}}{Fator que indica a qual conjunto a observação
+#'     pertence.}
 #'
-#'     \item{\code{x}}{Valores da coluna X}
+#'     \item{\code{x}}{Variável independente, sem interpretação.}
 #'
-#'     \item{\code{y}}{Valores da coluna Y}
+#'     \item{\code{y}}{Variável dependente, sem interpretação.}
 #'
 #' }
 #'
-#' @keywords amostra
+#' @details Este conjunto de dados agrupa dados onde pressupõe-se que
+#'     sejam de populações distintas. Portanto ao utilizá-lo separe-o
+#'     com base na variável \code{conj}.
+#'
+#' @keywords TODO
 #'
 #' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino,
 #'     H. (2008). Análise de modelos de regressão linear com aplicações
@@ -27,6 +33,11 @@
 #'
 #' data(CharnetEx1.17)
 #'
-#' plot(CharnetEx1.17)
+#' library(ggplot2)
+#'
+#' ggplot(CharnetEx1.17, aes(x = x, y = y)) +
+#'     geom_point() +
+#'     facet_grid(~conj, scales = "free_x") +
+#'     stat_smooth(method = "lm")
 #' 
 NULL
