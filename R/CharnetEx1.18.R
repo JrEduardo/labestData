bb3bd66d930f1fd6b01121c2989918881733e963
Eduardo Junior <edujrrib@gmail.com>
2016-03-25 20:59:23 -0300

    Correções no para dataset Ex1.18
    - Capitalização do @title
    - Indentação no @description
    - Altera @examples representando graficamente o proposto no exercício

--- R/CharnetEx1.18.R
+++ R/CharnetEx1.18.R
@@ -1,9 +1,9 @@
 #' @name CharnetEx1.18
 #' 
-#' @title Tempo e temperatura de uma reação química
+#' @title Tempo e Temperatura de uma Reação Química
 #' 
-#' @description Registro de 35 tempos de reação química em 7 
-#'              temperaturas diferentes
+#' @description Registro de 35 tempos de reação química em 7
+#'     temperaturas diferentes.
 #'
 #' @format Um \code{data.frame} com 2 colunas e 35 linhas.
 #'
@@ -26,6 +26,23 @@
 #'
 #' data(CharnetEx1.18)
 #'
-#' plot(CharnetEx1.18)
+#' library(ggplot2)
+#'
+#' with(CharnetEx1.18, {
+#'     mu <- aggregate(tempo, list(temp), mean)
+#'     des <- aggregate(tempo, list(temp), sd)
+#'     (da <<- data.frame(x = mu$G, mu = mu$x, sd = des$x))
+#' })
+#'
+#' ggplot(CharnetEx1.18, aes(x = temp, y = tempo)) +
+#'     geom_point() +
+#'     geom_point(
+#'         aes(x = x - 1, y = mu), data = da,
+#'         col = 4, shape = 15, size = 2.5) +
+#'     geom_segment(
+#'         aes(x = x - 1, y = mu - sd, xend = x - 1, yend = mu + sd),
+#'         arrow = arrow(angle = 90, length = unit(0.05, "inches"),
+#'                       ends = "both"), data = da, col = 4) +
+#'     geom_smooth(method = "lm", se = FALSE)
 #'
 NULL
