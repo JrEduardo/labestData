3591cf913949ae14f2726a12ca82fb40c0670108
Alcides Conte Neto <neto_conte@hotmail.com>
2016-03-25 22:27:35 -0300

    Adicionado e Documentado Tb3.[5-6]

---
+++ R/DemetrioTb3.5.R
@@ -0,0 +1,32 @@
+#' @name DemetrioTb3.5
+#' @title Duas variáveis explicativas
+#' 
+#' @description Dados para exercício.
+#' 
+#' @format Um \code{data.frame} de 6 linhas e 3 colunas.
+#' 
+#' \describe{
+#' 
+#'     \item{\code{x1}}{ Informação indisponível.}
+#'     
+#'     \item{\code{x2}}{ Informação indisponível.}
+#'     
+#'     \item{\code{y}{ Informação indisponível.}
+#' 
+#' }
+#' 
+#' @keywords TODO
+#' 
+#' @source Demétrio, C. G. B., & Zocchi, S. S. (2011). Modelos de
+#'     Regressão. Piracicaba: ESALQ. (Tabela 3.5 pág. 99)
+#' 
+#' @examples 
+#' 
+#' data(DemetrioTb3.5)
+#' 
+#' pairs(~ x1 + x2 + y , data = DemetrioTb3.5,
+#'      main = "Dispersão duas a duas")
+#'
+#' cor(DemetrioTb3.5)
+#'
+NULL
---
+++ R/DemetrioTb3.6.R
@@ -0,0 +1,34 @@
+#' @name DemetrioTb3.6
+#' @title Três variáveis explicativas
+#' 
+#' @description Dados para exercício.
+#' 
+#' @format Um \code{data.frame} de 14 linhas e 4 colunas.
+#' 
+#' \describe{
+#' 
+#'     \item{\code{x1}}{ Informação indisponível.}
+#'     
+#'     \item{\code{x2}}{ Informação indisponível.}
+#'     
+#'     \item{\code{x3}}{ Informação indisponível.}
+#'     
+#'     \item{\code{y}{ Informação indisponível.}
+#' 
+#' }
+#' 
+#' @keywords TODO
+#' 
+#' @source Demétrio, C. G. B., & Zocchi, S. S. (2011). Modelos de
+#'     Regressão. Piracicaba: ESALQ. (Tabela 3.6 pág. 99)
+#' 
+#' @examples 
+#' 
+#' data(DemetrioTb3.6)
+#' 
+#' pairs(~ x1 + x2 + x3 + y , data = DemetrioTb3.6,
+#'      main = "Dispersão duas a duas")
+#'
+#' cor(DemetrioTb3.6)
+#'
+NULL
