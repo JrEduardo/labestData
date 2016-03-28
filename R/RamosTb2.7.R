a99cce6064b43c8c5f2a8dda2549b8786eb06fd8
Jhenifer <jhenicaet@gmail.com>
2016-03-28 18:16:16 -0300

    Adiciona documentação RamosTb2.7

---
+++ R/RamosTb2.7.R
@@ -0,0 +1,33 @@
+#' @name RamosTb2.7
+#' @title Processo de fundição de magnésio
+#' @description Dados referente a um processo de fundição
+#'     de magnésio, onde a resposta é a recuperação do mental 
+#'     e aos valores do fluxo de regeneração adicionada.
+#'
+#' @format Um \code{data.frame} com 7 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{num}}{Força da resistência.}
+#'
+#' \item{\code{fluxo}}{Fluxo de recuperação do metal no processo
+#'     de fundição.}
+#'
+#' \item{\code{recupe}}{Recuperação do metal.}
+#'
+#' }
+#' @keywords TODO
+#' @source Ramos, E. M. L. S., et al.(2013). 
+#'     Controle Estatístico da Qualidade (1th ed.). 
+#'     Porto Alegre, RS: Bookman. (pg 30).
+#'      
+#' @examples
+#'
+#' data(RamosTb2.7)
+#'
+#' library (lattice)
+#'
+#' xyplot(recupe ~ fluxo ,pch=20,
+#'      RamosTb2.7, type =c( "g","a","p"))
+#'
+NULL
