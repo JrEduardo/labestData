ff097889f206efe659e0f8d3f3ed7a4e3be1afbf
Jhenifer <jhenicaet@gmail.com>
2016-03-29 14:26:24 -0300

    Add tabela 2.3

---
+++ R/EpprechtTb2.3.R
@@ -0,0 +1,30 @@
+#' @name EpprechtTb2.3
+#' @title Dados referentes à quantidade de leite
+#' @description Dados referentes à variável X que representa o volume de 
+#'     leite presente em um pacote, sendo ideal que em cada pacote 
+#'     houvesse exatamente 1000 ml de leite. Os dados são extraídos de 
+#'     um processo isento de causas especiais.
+#' 
+#' @format Um \code{data.frame} com 100 observações e 2 variáveis, 
+#'     em que
+#' 
+#' \describe{
+#'
+#' \item{\code{id}}{Identificação do pacote.}
+#' 
+#' \item{\code{X}}{Volume de leite presente no pacote (ml).}
+#' 
+#' }
+#'
+#' @keywords variabilidade
+#' @source Costa, A. F. B., Epprecht, E. K., Carpinetti, L. (2010).
+#'      Controle Estatístico de Qualidade (2nd ed.). São Paulo - SP:
+#'      Editora Atlas. (pg 35).
+#' 
+#' @examples
+#'
+#' data(EpprechtTb2.3) 
+#' 
+#' hist(EpprechtTb2.3$x)
+#'                   
+NULL
