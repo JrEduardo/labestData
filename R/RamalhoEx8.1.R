8ae1a38f47a3c94f5673d5eeeabdff94f6498ac2
Walmes Zeviani <walmes@ufpr.br>
2016-03-30 23:00:11 -0300

    Correçoes no Ex8.1.

--- R/RamalhoEx8.1.R
+++ R/RamalhoEx8.1.R
@@ -1,35 +1,44 @@
 #' @name RamalhoEx8.1
-#' @title Produção de grãos de arroz
+#' @title Produção de Grãos de Arroz em Municípios de Minas Gerais
 #' @description Experimento da produção de grãos de arroz, conduzido em
-#'     três locais do Estado de Minas Gerais (Lambari, Lavras e 
+#'     três locais do Estado de Minas Gerais (Lambari, Lavras e
 #'     Felixlândia). Delineamento em blocos casualizados.
 #' @format Um \code{data.frame} com 90 observações e 4 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{\code{cult}}{Fator de 10 níveis de cultivares de arroz.}
+#' \item{\code{cult}}{Fator de níveis nominais que representa as
+#'     cultivares de arroz.}
 #'
-#' \item{\code{local}}{Fator de 3 níveis nominais, municípios onde 
-#'     foi realizado o experimento.}
+#' \item{\code{local}}{Fator de 3 níveis nominais, municípios de Minas
+#'     Gerais onde foi realizado o experimento.}
 #'
-#' \item{\code{rept}}{Fator de 3 níveis do experimento, usado para 
+#' \item{\code{rept}}{Fator de 3 níveis do experimento, usado para
 #'     controle local.}
 #'
-#' \item{\code{prod}}{Produção de grãos de arroz, medido em kg/ha nas 
-#'     unidades experimentais.}
+#' \item{\code{prod}}{Produção de grãos de arroz, medido em kg
+#'     ha\eqn{^{-1}} nas unidades experimentais.}
 #'
 #' }
 #' @keywords DBC
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005).
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
 #'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
-#'     Lavras: UFLA. (pg 132)
+#'     Lavras: UFLA. (Exercício 8.1, pág. 132)
 #' @examples
 #'
-#' aggregate(prod ~ local,  data = RamalhoEx8.1,
+#' data(RamalhoEx8.1)
+#'
+#' str(RamalhoEx8.1)
+#'
+#' with(RamalhoEx8.1, tapply(prod, list(cult, local), FUN = mean))
+#'
+#' aggregate(prod ~ local + cult, data = RamalhoEx8.1,
 #'           FUN = function(x) { c(mean = mean(x), var = var(x)) })
 #'
-#' boxplot(prod ~ cult, data = RamalhoEx8.1,
-#'      xlab = "Cultivar de arroz",
-#'      ylab = "Produção")
-#' 
+#' library(lattice)
+#'
+#' xyplot(prod ~ cult | local, data = RamalhoEx8.1,
+#'        xlab = "Cultivar de arroz",
+#'        ylab = "Produção")
+#'
 NULL
