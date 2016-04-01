b4a7dd3e9ea67ae9001363c69371de3d631e1bb1
Jhenifer <jhenicaet@gmail.com>
2016-04-01 16:58:07 -0300

    Adiciona modificações sugeridas na tabela RamalhoEx7.10

--- R/RamalhoEx7.10.R
+++ R/RamalhoEx7.10.R
@@ -4,17 +4,19 @@
 #'     avaliação de 18 populações \eqn{F_{2}} e duas linhagens
 #'     utilizadas como testemunhas. O Experimento foi realizado em
 #'     blocos casualizados.
-#' @format Um \code{data.frame} com 540 observações e 4 variáveis, em
+#' @format Um \code{data.frame} com 600 observações e 4 variáveis, em
 #'     que
 #'
 #' \describe{
 #'
-#' \item{\code{pop}}{População \eqn{F_{2}} de arroz.}
+#' \item{\code{pop}}{População \eqn{F_{2}} da linhagem de arroz com 18 
+#'     níveis e 2 testemunhas.}
 #'
-#' \item{\code{rept}}{Fator que indica se a produção veio da primeira ou
-#'     segunda linhagem.}
+#' \item{\code{rept}}{Fator que indica a repetição da combinação entre 
+#'     planta e linhagem.}
 #'
-#' \item{\code{plant}}{Inteiro que representa o arroz no experimento.}
+#' \item{\code{plant}}{Inteiro que representa a planta de arroz avaliada
+#'     no experimento.}
 #'
 #' \item{\code{prod}}{Produção de arroz, em g/planta.}
 #'
