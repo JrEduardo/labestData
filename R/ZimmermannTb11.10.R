1b0c49b4ecd3d62c5af014e53bcad705bd21c24b
Bruna Wundervald <brunadaviesw@gmail.com>
2016-04-01 18:30:19 -0300

    Correções do cap 11.

--- R/ZimmermannTb11.10.R
+++ R/ZimmermannTb11.10.R
@@ -1,5 +1,5 @@
 #' @name ZimmermannTb11.10
-#' @title Experimento fatorial com feijão
+#' @title Espaçamento Entre Linhas e Densidade no Plantio de Feijão
 #' @description Dados de um experimento fatorial \eqn{3^2}, com a cultura do 
 #'     feijoeiro testando espaçamento entre linhas de plantio (0.45m, 0.6m e
 #'     0.75m) e densidade de plantio (7, 10 e 13 sementes por metro). Os dados
@@ -17,13 +17,13 @@
 #'    
 #' \item{\code{bloco}}{Fator de níveis numéricos. Identifica o bloco da 
 #'     repetição.}
-#'
-#' \item{\code{iden}}{Fator de níveis numéricos. É a identificação que 
-#'     representa o tratamento.}
 #'     
 #' \item{\code{altura}}{Altura de plantas, em cm.}
 #'
-#' @keywords DBC confundimento FAT
+#' \item{\code{iden}}{Fator de níveis numéricos. É a identificação que 
+#'     representa o tratamento.}
+#'
+#' @keywords DBC FAT
 #' @source Zimmermann, F. J. (2004). Estatística aplicada à pesquisa
 #'     agrícola (1st ed.). Santo Antônio de Goiás, GO: Embrapa Arroz e
 #'     Feijão. (pg 231)
@@ -33,19 +33,18 @@
 #'
 #' data(ZimmermannTb11.10)
 #'
+#' str(ZimmermannTb11.10)
+
+#'
 #' aggregate(altura ~ bloco, data = ZimmermannTb11.10, 
 #'           FUN = function(x) { c(mean = mean(x), var = var(x)) })
 #' 
 #' aggregate(altura ~ iden, data = ZimmermannTb11.10, 
 #'           FUN = function(x) { c(mean = mean(x), var = var(x)) })
 #' 
-#' xyplot(altura ~ iden + rept, groups = bloco, data = ZimmermannTb11.10, 
-#'        type=c("p","a"),
-#'        xlab="Fatores", ylab="Produção", 
-#'        main="Análise de um fatorial com confundimento")
+#' xyplot(altura ~ iden | rept, groups = bloco, data = ZimmermannTb11.10, 
+#'        type = c("p","a"),
+#'        ylab = "Identificação do Tratamento", 
+#'        xlab="Altura de Plantas, em cm")
 #'        
-#' with(ZimmermannTb11.10, interaction.plot(bloco, rept, altura, lty = c(2,3), 
-#'                                          col = 4:7, 
-#'                                          ylab = "Altura de Plantas",
-#'                                          xlab = "Bloco"))
 NULL
