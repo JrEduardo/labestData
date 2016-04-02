ae474d88ac7a4a228eaac572ce1c1d5a5f7a62c5
Alcides Conte Neto <neto_conte@hotmail.com>
2016-04-02 12:19:34 -0300

    Adicionado e Documentado Tb10.2

---
+++ R/DemetrioTb10.2.R
@@ -0,0 +1,31 @@
+#' @name DemetrioTb5.1
+#' @title Nicotiana
+#' 
+#' @description Dados referentes ao comprimento do caule,
+#'     do ramo e do caule basal da Nicotiana. 
+#' 
+#' @format Um \code{data.frame} de 18 linhas e 3 colunas.
+#' 
+#' \describe{
+#' 
+#'     \item{\code{caule}}{ Comprimento do caule. }
+#' 
+#'     \item{\code{ramo}}{ Comprimento do ramo. }
+#'     
+#'     \item{\code{basal}}{ Comprimento do caule basal. }
+#'     
+#' }
+#' 
+#' @keywords RM
+#' 
+#' @source Demétrio, C. G. B., & Zocchi, S. S. (2011). Modelos de
+#'     Regressão. Piracicaba: ESALQ. (Tabela 10.2 pág. 161)
+#' 
+#' @examples
+#' 
+#' data(DemetrioTb10.2)
+#' 
+#' pairs(~ caule + basal + ramo , data = DemetrioTb10.2,
+#'      main = "Dispersão duas a duas")
+#'
+NULL
