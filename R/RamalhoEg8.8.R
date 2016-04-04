88c1342fbd92660fe3d7eb67892fdfe454e04769
Jhenifer <jhenicaet@gmail.com>
2016-04-04 14:34:40 -0300

    Adiciona modificãções na tabela RamalhoEg8.8

--- R/RamalhoEg8.8.R
+++ R/RamalhoEg8.8.R
@@ -1,36 +1,39 @@
 #' @name RamalhoEg8.8
-#' @title Produção de grãos de feijão
+#' @title Produção de Grãos de Feijão
 #' @description Dados da produção de grãos de feijão obtidos por Corte 
 #'     et al. (2002) relativos à avaliação de 6 populações segregantes 
 #'     avaliadas nas gerções F_{2}, F_{3} e F_{4} com diferentes números 
-#'     de repetições. Delineamento em blocos casualizados.
+#'     de blocos. Delineamento em blocos casualizados.
 #' @format Um \code{data.frame} com 72 observações e 4 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{\code{exp}}{Fator de 3 níveis que representa a gerações de 
+#' \item{\code{ger}}{Fator de 3 níveis que representa a gerações de 
 #'     feijão.}
 #'
-#' \item{\code{rept}}{Fator de 3 níveis do experimento, usado para 
-#'     fazer controle local.}
+#' \item{\code{bloc}}{Fator usado para fazer controle local.}
 #'
-#' \item{\code{trat}}{Fator de 6 níveis que representa as populações das
+#' \item{\code{pop}}{Fator de 6 níveis que representa as populações das
 #'     gerações do experimento.}
 #'
 #' \item{\code{prod}}{Produção de grãos de feijão, medidos em kg/ha nas
 #'     unidades experimentais.}
 #'
 #' }
-#' @keywords DBC
-#' @source Ramalho, M. A. P., Ferreira, D. F. & Oliveira, A. C. (2005).
+#' @keywords DBC FAT2
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
 #'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
 #'     Lavras: UFLA. (pg 123)
+#'     
+#'     Corte, H. R., Ramalho, M. A. P., Gonçalves, F. M. A., Abreu. A de
+#'     F. B. Natural Selection for Grain Yield in Dry Bean Population 
+#'     Bred by the Bulk Method. Euphytica. Wageningen. v.123, n.3. 2002 
 #' @examples
 #'
 #' library(lattice)
 #' 
-#' xyplot(prod ~ trat, data = RamalhoEg8.8,
-#'        groups = rept,
+#' xyplot(prod ~ pop, data = RamalhoEg8.8,
+#'        groups = bloc,
 #'        jitter.x = TRUE,
 #'        xlab = "Tratamento",
 #'        ylab = "Produção em (kg/ha)")
