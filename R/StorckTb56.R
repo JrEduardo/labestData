4854cfda4401304d0eab85ad511f659b5fb33012
Ludmila42 <monica.ludmila@gmail.com>
2016-04-05 08:00:54 -0300

    Documenta tabela StorckTb56

---
+++ R/StorckTb56.R
@@ -0,0 +1,26 @@
+#' @name StorckTb56
+#' @title 
+#' @description
+#' @format Um \code{data.frame} com 48 observa√ß√µes e 4 vari√°veis.
+#' \describe{
+#' 
+#' \item{\code{trat.A}}{  }
+#' 
+#' \item{\code{trat.D}}{  }
+#' 
+#' \item{\code{blocos}}{  }
+#' 
+#' \item{\code{result}}{  }
+#' 
+#' }
+#' @keywords
+#' @source Storck, L., & Garcia, B. C., & Lopes, S. J., & Estefanel, 
+#' V. (2011). ExperimentaÁ„o Vegetal (3th ed.). Santa Maria, RS: UFSM.
+#' (Tabela 56, p·g. 134.)
+#' @examples
+#' 
+#' data(StorckTb56)
+#' 
+#' plot(trat.A ~ trat.D, data = StorckTb56)
+#' 
+NULL
---
+++ R/StorckTb67.R
@@ -0,0 +1,18 @@
+#' @name StorckTb67
+#' @title
+#' @description
+#' @format Um \code{data.frame} com 60 observações e 4 variáveis.
+#' \describe{
+#' 
+#' \item{\code{trat.A}}{  }
+#' 
+#' \item{\code{trat.D}}{  }
+#' 
+#' \item{\code{blocos}}{  }
+#' 
+#' \item{\code{result}}{  }
+#' 
+#' }
+#' @keywords
+#' @source ???
+#' @examples
