e06394f5fedc5b407590380aadb1c48b0b6c8bce
Ludmila42 <monica.ludmila@gmail.com>
2016-04-05 17:53:20 -0300

    Documenta tabela StorckTb74

---
+++ R/StorckEg2.3.5.R
@@ -0,0 +1,20 @@
+#' @name StorckEg2.3.5
+#' @title Exemplo de Teste de Hipóteses e Interpretação
+#' @description Sejam os resultados de um experimento no delineamento 
+#' quadraddo latino, em que foram avaliados o rendimento (t/ha) de 
+#' quatro cultivares (A, B, C e D) de alho.
+#' @format Um \code{data.frame} com 48 observações e 4 variáveis.
+#' \describe{
+#' 
+#' \item{\code{trat.A}}{  }
+#' 
+#' \item{\code{trat.D}}{  }
+#' 
+#' \item{\code{blocos}}{  }
+#' 
+#' \item{\code{result}}{  }
+#' 
+#' }
+#' @keywords
+#' @source ???
+#' @examples
---
+++ R/StorckTb88.R
@@ -0,0 +1,12 @@
+#' @name StorckTb88
+#' @title
+#' @description
+#' @format Um \code{data.frame} com 75 observações e 1 variáveis.
+#' \describe{
+#' 
+#' \item{\code{trat.A.trat.D.blocos.result}}{  }
+#' 
+#' }
+#' @keywords
+#' @source ???
+#' @examples
