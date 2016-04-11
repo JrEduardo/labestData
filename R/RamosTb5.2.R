6a25967401cf0edda724f59523325d588a0512c8
Ângela Luiza Cunha Legey <angelalegey@gmail.com>
2016-04-11 11:31:34 -0300

    Incorpora alterações solicitadas Ramos5.2

--- R/RamosTb5.2.R
+++ R/RamosTb5.2.R
@@ -1,7 +1,7 @@
 #' @name RamosTb5.2
 #' @title Itens não conformes no processo produtivo de ovos de galinha
 #' @description Número de itens não conformes de um processo  
-#'     produtivo de ovos de galinha, com 15 amostras e subamostras de 
+#'     produtivo de ovos de galinha, com 30 amostras de 
 #'     tamanhos diferentes. 
 #'
 #' @format Um \code{data.frame} com 30 observações e 3 variáveis, em que
@@ -10,9 +10,9 @@
 #'
 #' \item{\code{amostra}}{Número da amostra.}
 #'
-#' \item{\code{subamostra}}{Número de subamostras conforme a amostra.}
+#' \item{\code{tamamostra}}{Número de unidades amostradas.}
 #'
-#' \item{\code{naoconf}}{Número de unidades não conformes na amostra.}
+#' \item{\code{naoconf}}{Número de ovos não conformes na amostra.}
 #'     
 #' }
 #' @keywords CEQ
@@ -26,8 +26,6 @@
 #' 
 #' library(qcc)
 #'
-#' obj <- qcc.groups(RamosTb5.2$naoconf, RamosTb5.2$amostra)
-#' 
-#' qcc(obj, type = "c", nsigmas = 3)
+#' qcc(RamosTb5.2$naoconf, sizes = RamosTb5.2$tamamostra, type = "p")
 #'
 NULL
