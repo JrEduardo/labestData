8036c0b77bbba96dfd1af67c42fd12c4487a44fd
Jhenifer <jhenicaet@gmail.com>
2016-04-12 11:34:23 -0300

    Adiciona tabela RamalhoEg12.10 ao pacote

---
+++ R/RamalhoEg12.10.R
@@ -0,0 +1,33 @@
+#' @name RamalhoEg12.10
+#' @title Crescimento para o Feijoeiro
+#' @description Dados referentes ao cruzamento dialelo realizado por
+#'     Arriel et al. (1993), em que foi avaliado o número de dias para o
+#'     florescimento do feijoeiro.
+#' @format Um \code{data.frame} com 9 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{prog1}}{Fator que indica o progenitor 1 do cruzamento.}
+#'
+#' \item{\code{prog2}}{Fator que indica o progenitor 2 do cruzamento.}
+#'
+#' \item{\code{dias}}{Número médio de dias para o florescimento do 
+#'     feijoeiro.}
+#'
+#' }
+#' @keywords RM
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
+#'     Lavras: UFLA. (Exemplo 12.10, pág. 227)
+#'     
+#'     Arriel, E., Ramalho, M. A. P., Pacheco, C. A. P. Expected and 
+#'     Realized Gains in the CMS-39 Maize Population after three Cycles
+#'     of Selection. Revista Brasileira de Genética. Ribeirão Preto. 
+#'     v.16, n.4, p.1013. dez. 1993
+#' @examples
+#'
+#' data(RamalhoEg12.10)
+#'
+#' plot(RamalhoEg12.10)
+#'
+NULL
