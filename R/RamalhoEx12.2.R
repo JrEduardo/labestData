bd15172ba8dc0b907900f18f08f2d7ebaddd465e
Jhenifer <jhenicaet@gmail.com>
2016-04-12 10:00:25 -0300

    Adiciona tabela RamalhoEx12.2 ao pacote

---
+++ R/RamalhoEx12.2.R
@@ -0,0 +1,28 @@
+#' @name RamalhoEx12.2
+#' @title Dados Genéricos para Regressão Múltipla
+#' @description Conjunto de dados apresentados para exercício. Objetivo
+#' do exercício é estimar a equação de regressão múltipla.
+#' @format Um \code{data.frame} com ?? observações e 5 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{cult}}{Fator que indica as cultivares.}
+#'
+#' \item{\code{prod}}{Produtividade de grãos, medidos em kg/ha.}
+#'
+#' \item{\code{alt}}{Altura da planta, em centímetros.}
+#'
+#' \item{\code{perf}}{Número de perfilhos.}
+#'
+#' }
+#' @keywords RM
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
+#'     Lavras: UFLA. (Exercício 12.2, pág. 231)
+#' @examples
+#'
+#' data(RamalhoEx12.2)
+#'
+#' plot(RamalhoEx12.2)
+#'
+NULL
