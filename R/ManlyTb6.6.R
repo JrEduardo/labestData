e5fc406c0e435090cea625461aa24df407da3aa4
Gabriel Sartori Klostermann <gabrielsartori2008@gmail.com>
2016-04-12 10:03:18 -0300

    Correções da Tabela 6.6

--- R/ManlyTb6.6.R
+++ R/ManlyTb6.6.R
@@ -4,15 +4,15 @@
 #' @description Dimensões de 25 taças de cerâmicas escavadas de lugares pré-histórico 
 #' na Tailândia.
 #'
-#' @format Um \code{data.frame} com 25 registros em 5 variáveis.
+#' @format Um \code{data.frame} com 25 registros em 6 variáveis.
 #' 
 #' \describe{
 #'
 #' A figura apresentada descreve as cinco variáveis, correspondentes às
 #' dimensões das taças.
 #' 
-#' \if{html}{\figure{ManlyTb6.6.jpg}{options: width="250px"}}
-#' \if{latex}{\figure{ManlyTb6.6.jpg}{options: width=1.75in}} 
+#' \if{html}{\figure{ManlyTb6-6.jpg}{options: width="250px"}}
+#' \if{latex}{\figure{ManlyTb6-6.jpg}{options: width=1.75in}} 
 #'}
 #' @keywords AnaComp
 #' @source Manly, B. J. F. (2005). Métodos Estatísticos Multivariados- uma introdução.
@@ -21,7 +21,7 @@
 #'
 #' data(ManlyTb6.6)
 #'
-#' pairs(~ x1 + x2 + x3 + x4 + x5,
+#' pairs(~ x1 + x2 + x3 + x4 + x5 + x6,
 #'       data = ManlyTb6.6, 
 #'       main="Matriz de diapersão para as variáveis de medida das taças")
 NULL
