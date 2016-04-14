0ff6488921f5174036fb9b1b9b436dbf953086ed
Ângela Luiza Cunha Legey <angelalegey@gmail.com>
2016-04-14 16:09:24 -0300

    Adiciona documentação RamosAnC6

---
+++ R/RamosAnC6.R
@@ -0,0 +1,30 @@
+#' @name RamosAnC6
+#' @title Processo de fabricação de panelas
+#' @description Conjunto de dados de um processo de fabricação  
+#'     de panelas em 30 amostras com tamanhos diferentes.
+#'
+#' @format Um \code{data.frame} com 30 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{amostra}}{Número da amostra.}
+#'
+#' \item{\code{tamamostra}}{Tamanho da amostra.}
+#'
+#' \item{\code{naoconf}}{Número de panelas não conformes.}
+#'     
+#' }
+#' @keywords CEQ
+#' @source Ramos, E. M. L. S., et al.(2013). 
+#'     Controle Estatístico da Qualidade (1th ed.). 
+#'     Porto Alegre, RS: Bookman. (pg 136).
+#'      
+#' @examples
+#'
+#' data(RamosAnC6)
+#'
+#' library(qcc)
+#'
+#' qcc(RamosAnC6$naoconf, sizes = RamosAnC6$tamamostra, type = "u")
+#'
+NULL
