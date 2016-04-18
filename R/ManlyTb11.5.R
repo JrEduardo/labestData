18d1d48c01050e32dddfc14c53cf07c092d0312c
Cesar Taconeli <taconeli@ufpr.br>
2016-04-18 15:26:21 -0300

    Finaliza correções e encaminha para merge gabriel60

--- R/ManlyTb11.5.R
+++ R/ManlyTb11.5.R
@@ -1,24 +1,22 @@
 #' @name ManlyTb11.5
-#' @title Comportamento da votação de parlamentares
+#' @title Votações de parlamentares
 #'
-#' @description Número de votos discordantes entre os parlamentares de Nova Jersey em leis que concernem sobre problemas ambientais
+#' @description Número de votos discordantes entre os parlamentares de 
+#' Nova Jersey em leis referentes a problemas ambientais
 #'
-#' @format Um \code{matrix} com 15 parlamentares
+#' @format Uma matriz com os números de votos discordantes entre 15
+#' parlamentares de Nova Jersey.
 #' 
-#' \describe{
-#'
-#' \item{\code{Colnames}}{Identificação dos parlamentares.}
-#' 
-#' \item{\code{Rownames}}{Identificação das parlamentares.}
-#' 
-#' }
 #' @keywords EM
-#' @source Manly, B. J. F. (2005). Métodos Estatísticos Multivariados - uma introdução.
-#'      Porto Alegre, RS: Bookman (pg 185)
+#' @source Manly, B. J. F. (2005). Métodos Estatísticos Multivariados 
+#'      - uma introdução. Porto Alegre, RS: Bookman (pg 185)
+#'      
 #' @examples
 #'
 #' data(ManlyTb11.5)
 #' 
-#' levelplot(ManlyTb11.5, xlab = "", ylab = "", main = "Gráfico das distâncias",
-#'  scales = list(x = list(rot=90)))
+#' require(lattice)
+#' 
+#' levelplot(ManlyTb11.5, xlab = "", ylab = "", main = "Gráfico das 
+#' distâncias entre parlamentares", scales = list(x = list(rot=90)))
 NULL
