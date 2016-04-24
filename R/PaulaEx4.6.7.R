34ae7d9da47c4be37834f0be70640b668cfed59d
Eduardo Junior <edujrrib@gmail.com>
2016-04-24 19:46:23 -0300

    Adiciona dataset PaulaEx4.6.7

---
+++ R/PaulaEx4.6.7.R
@@ -0,0 +1,32 @@
+#' @name PaulaEx4.6.7
+#' @title Número de Falhas em Peças de Tecido
+#' @description Dados referentes à produção de peças de tecido em uma
+#'     determinada fábrica. A fábrica registra o comprimento da peça
+#'     produzida e o número de falhas encontradas na peça.
+#' @format Um \code{data.frame} com 32 observações e 2 variáveis.
+#' \describe{
+#' 
+#' \item{\code{comp}}{Comprimento da peça de tecido produzida, em metros.}
+#' 
+#' \item{\code{nfalhas}}{Número de falhas encontradas na peça.}
+#' 
+#' }
+#' @keywords contagem superdispersão
+#' @source Paula, G. A. (2004). Modelos de regressão: com apoio
+#'     computacional. São Paulo, SP: IME-USP. (Exercício 4.6.7 pág. 343)
+#'
+#' @references Hinde, J. (1982). Compound Poisson Regression Models in R
+#'     (Gilchrist ed.). Springer, New York.
+#' @examples
+#'
+#' data(PaulaEx4.6.7)
+#' 
+#' str(PaulaEx4.6.7)
+#'
+#' library(lattice)
+#' xyplot(nfalhas ~ comp, data = PaulaEx4.6.7,
+#'        type = c("p", "g", "smooth"))
+#' 
+#' histogram( ~nfalhas/comp, data = PaulaEx4.6.7)
+#'
+NULL
