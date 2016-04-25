5017204207e84b737283b2d89b018c7f89e08d0d
Alcides Conte Neto <neto_conte@hotmail.com>
2016-04-25 10:55:29 -0300

    Adicionado e Documentado Tb7.7

---
+++ R/VieiraTb7.7.R
@@ -0,0 +1,29 @@
+#' @name VieiraTb7.7
+#' @title Teste de conhecimento
+#' @description Dados referentes às notas dos alunos em um teste de
+#'      conhecimento segundo a fonte de informação (tratamento).
+#' @format Um \code{data.frame} com 24 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{bloco}}{Grupo ao qual a observação pertence.}
+#'
+#' \item{\code{tratamento}}{Tratamento ao qual o aluno foi submetido.}
+#'
+#' \item{\code{valor}}{Nota do aluno no teste.}
+#'
+#' }
+#' @keywords TODO
+#' @source Vieira, S.(1999). Estatística experimental (2th ed.). 
+#'     São Paulo, SP: Atlas. (Tabela 7.7 pág. 81).
+#' @examples
+#'
+#' library(lattice)
+#'
+#' data(VieiraTb7.7)
+#'
+#' xyplot(valor ~ tratamento, group = bloco, data = VieiraTb7.7,
+#'        ylab = "Nota do Aluno",
+#'        xlab = "Tratamento")
+#'
+NULL
