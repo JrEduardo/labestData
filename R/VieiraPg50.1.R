9c6411a9d0a4c1d20cd9dd16e2b327028822539e
Walmes Zeviani <walmes@ufpr.br>
2016-04-26 09:58:43 -0300

    Correçoes minimas nos arquivos para passar adiante.

--- R/VieiraPg50.1.R
+++ R/VieiraPg50.1.R
@@ -1,29 +1,30 @@
 #' @name VieiraPg50.1
-#' @title Experimento inteiramente casualizado
-#' @description Experimento inteiramente casualizado com 
-#'      5 tratamentos e resposta em inteiro.
-#'
+#' @title Experimento em Delineamento Inteiramente Casualizado
+#' @description Experimento em delineamento inteiramente casualizado que
+#'     estudou o efeito de um fator de 5 níveis categóricos em uma
+#'     resposta na escala dos números inteiros.
 #' @format Um \code{data.frame} com 30 observações e 2 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{\code{trat}}{Fator de 5 níveis de tratamento.}
+#' \item{\code{trat}}{Fator categórico de 5 níveis.}
 #'
-#' \item{\code{resp}}{Resposta em números inteiros.}
+#' \item{\code{resp}}{Variável resposta em números inteiros.}
 #'
 #' }
 #' @keywords DIC
-#' @source Vieira, S.(1999). 
-#'     Estatística experimental (2th ed.). 
-#'     São Paulo, SP: Atlas. (pg 50 - Ex 2).
-#'      
+#' @source Vieira, S. (1999). Estatística experimental (2th ed.). São
+#'     Paulo, SP: Atlas. (pág 50, exercício 2).
 #' @examples
 #'
 #' data(VieiraPg50.1)
-#' 
+#'
+#' str(VieiraPg50.1)
+#'
 #' library(lattice)
-#' 
+#'
 #' xyplot(resp ~ trat, data = VieiraPg50.1,
+#'        jitter.x = TRUE,
 #'        xlab = "Tratamento",
 #'        ylab = "Resposta")
 #'
--- R/VieiraTb4.1.R
+++ R/VieiraTb4.1.R
@@ -1,29 +1,29 @@
 #' @name VieiraTb4.1
-#' @title Produção de milho segundo a variedade
-#' @description Experimento da produção de milho em kg/100 m\eqn{^{-2}}) 
-#'     segundo 4 tipos de variedades de milho em um experimento
+#' @title Produção de Variedades de Milho
+#' @description Experimento que mediu a produção, em kg/100
+#'     m\eqn{^{-2}}), de 4 variedades de milho em um delineamento
 #'     inteiramente casualizado.
-#'
 #' @format Um \code{data.frame} com 20 observações e 2 variáveis, em que
 #'
 #' \describe{
 #'
-#' \item{\code{varied}}{Fator de 4 níveis de variedade de milho.}
+#' \item{\code{varied}}{Fator categórico de 4 níveis que indica as
+#'     variedades de milho.}
 #'
-#' \item{\code{prod}}{Produção de milho, medidos kg/100 m\eqn{^{2}}) 
-#'     nas unidades experimentais.}
+#' \item{\code{prod}}{Produção de milho, medida em kg/100 m\eqn{^{2}}).}
 #'
 #' }
 #' @keywords DIC
-#' @source Vieira, S.(1999). 
-#'     Estatística experimental (2th ed.). 
-#'     São Paulo, SP: Atlas. (pg 44).
-#'      
+#' @source Vieira, S. (1999). Estatística experimental (2th ed.). São
+#'     Paulo, SP: Atlas. (pág 44, tabela 4.1).
 #' @examples
 #'
 #' data(VieiraTb4.1)
-#' 
+#'
+#' str(VieiraTb4.1)
+#'
 #' library(lattice)
+#'
 #' xyplot(prod ~ varied, data = VieiraTb4.1,
 #'       xlab = "Variedades",
 #'       ylab = "Produção de milho")
