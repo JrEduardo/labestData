4a57b068c60fcc148a8425f0bcc8cdb511f4fb40
Jhenifer <jhenicaet@gmail.com>
2016-05-02 16:38:29 -0300

    Adiciona tabela Ramalho13.15 ao pacote

---
+++ R/RamalhoTb13.15.R
@@ -0,0 +1,35 @@
+#' @name RamalhoTb13.15
+#' @title Produção de Raízes de Mandioca
+#' @description Experimento envolvendo a produção de ramas (parte aérea)
+#'     de 20 genótipos parentais (clones) de mandioca e de suas
+#'     respectivas filhas.
+#' @format Um \code{data.frame} com 20 observações e 3 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{clone}}{Fator de 20 níveis que indica o clone de
+#'     mandioca.}
+#'
+#' \item{\code{mae}}{Produção de ramas da planta mãe.}
+#'
+#' \item{\code{filha}}{Produção de ramas da planta filha.}
+#'
+#' }
+#' @keywords COV RS
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
+#'     Lavras: UFLA. (Tabela 13.15, pág. 252)
+#' @examples
+#'
+#' data(RamalhoTb13.15)
+#'
+#' str(RamalhoTb13.15)
+#'
+#' library(lattice)
+#'
+#' xyplot(filha ~ mae, data = RamalhoTb13.15,
+#'        type = c("p", "smooth", "g"), aspect = "iso",
+#'        xlab = "Produção de ramas na planta mãe",
+#'        ylab = "Produção de ramas na planta filha")
+#'
+NULL
