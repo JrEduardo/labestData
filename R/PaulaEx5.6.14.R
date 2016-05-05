a9938ff7a485e6b1ec72b2680bb8c29d8a36f197
Ângela Luiza Cunha Legey <angelalegey@gmail.com>
2016-05-05 15:59:02 -0300

    Adiciona referência PaulaEx5.6.14

--- R/PaulaEx5.6.14.R
+++ R/PaulaEx5.6.14.R
@@ -23,6 +23,11 @@
 #' @keywords quase-verossimilhança
 #' @source Paula, G. A. (2004). Modelos de regressão: com apoio
 #' computacional. São Paulo, SP: IME-USP. (Exercício 5.6.14, pág. 401)
+#' 
+#' @references Myers, R.H.; Montgomery, D. C. e Vining, G. G. (2002).
+#' Generalized Linear Models: With Applications in Engineering and the 
+#' Sciences. John Wiley, New York.
+#' 
 #' @examples
 #' 
 #' library(lattice)
