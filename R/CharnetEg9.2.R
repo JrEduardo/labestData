8dd7d11c9f51c3fbcc71207b70f3e6897e20353d
Daniel Ikenaga <di12@inf.ufpr.br>
2016-05-09 03:04:02 -0300

    Atualiza tabelas Cap 9

---
+++ R/CharnetEg9.2.R
@@ -0,0 +1,33 @@
+#' @name CharnetEg9.2
+#' 
+#' @title Relação entre Poluentes Atmosféricos
+#' 
+#' @description Relação de poluentes atmosféricos emitidos por carros
+#'     durante uma coleta realizada em sete períodos diferentes em um
+#'     grande centro.
+#'
+#' @format Um \code{data.frame} com 3 colunas e 7 linhas.
+#'
+#' \describe{
+#'
+#' \item{\code{y}}{Óxido de nitrogênio, sem especificação.}
+#' 
+#' \item{\code{x1}}{Monóxido de carbono, sem especificação.}
+#'
+#' \item{\code{x2}}{Hidrocarbonetos, sem especificação.}
+#'
+#' }
+#'
+#' @keywords TODO
+#'
+#' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino,
+#'     H. (2008). Análise de modelos de regressão linear com aplicações
+#'     (2nd ed., p. 356). SP: Editora Unicamp (Cap. 9, exem. 2, p. 216)
+#'
+#' @examples
+#'
+#' data(CharnetEg9.2)
+#'
+#' plot(CharnetEg9.2)
+#'
+NULL
---
+++ R/CharnetEx9.6.R
@@ -0,0 +1,29 @@
+#' @name CharnetEx9.6
+#' 
+#' @title Conjunto de Dados Qualquer
+#' 
+#' @description Conjunto de Dados Qualquer.
+#'
+#' @format Um \code{data.frame} com 2 colunas e 15 linhas.
+#'
+#' \describe{
+#'
+#' \item{\code{x}}{Dados do eixo x.}
+#'
+#' \item{\code{y}}{Dados do eixo y.}
+#'
+#' }
+#'
+#' @keywords TODO
+#'
+#' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino,
+#'     H. (2008). Análise de modelos de regressão linear com aplicações
+#'     (2nd ed., p. 356). SP: Editora Unicamp (Cap. 9, exer. 6, p. 230)
+#'
+#' @examples
+#'
+#' data(CharnetEx9.6)
+#'
+#' plot(CharnetEx9.6)
+#'
+NULL
