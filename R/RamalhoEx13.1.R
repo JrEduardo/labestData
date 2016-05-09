d36f9e3c0f1cf0f7f653a05c2b174da9c4ddd59d
Jhenifer <jhenicaet@gmail.com>
2016-05-09 14:37:56 -0300

    Adiciona tabela RamalhoEx13.1 ao pacote

---
+++ R/RamalhoEx13.1.R
@@ -0,0 +1,40 @@
+#' @name RamalhoEx13.1
+#' @title Avaliação de Clones de Eucaliptos
+#' @description Experimento referente a avaliação de clones de
+#'     eucaliptos, obtidos na Aracruz Celulose.
+#' @format Um \code{data.frame} com 20 observações e 5 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{clone}}{Fator de 20 níveis que representa o clone de
+#'     eucaliptos.}
+#'
+#' \item{\code{alt}}{Altura do eucalipto, medido em metros.}
+#'
+#' \item{\code{dap}}{\eqn{DAP^{\underline{1/}}} (diâmetro a altura do
+#'     peito).}
+#'
+#' \item{\code{broto}}{Número médio de brotos/árvore.}
+#'
+#' \item{\code{perc}}{Percentagem de enraizamento das estacas.}
+#'
+#' }
+#' @keywords COV
+#' @source Ramalho, M. A. P., Ferreira, D. F., Oliveira, A. C. (2005).
+#'     Experimentação em Genética e Melhoramento de Plantas (2th ed.).
+#'     Lavras: UFLA. (Exercício 13.1, pág. 255)
+#' @examples
+#'
+#' data(RamalhoEx13.1)
+#'
+#' str(RamalhoEx13.1)
+#' 
+#' library(lattice)
+#' 
+#' xyplot(alt ~ clone, data = RamalhoEx13.1,
+#'        type = c("p", "smooth", "g"), aspect = "iso")
+#'
+#' xyplot(dap ~ clone, data = RamalhoEx13.1,
+#'        type = c("p", "a"), aspect = "iso")
+#'
+NULL
