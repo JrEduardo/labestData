ffb31f44b2af5ffd4548a74400d87ee8e75851de
Daniel Ikenaga <di12@inf.ufpr.br>
2016-05-09 06:06:27 -0300

    Adiciona tabela faltante.

---
+++ R/CharnetEx11.2.R
@@ -0,0 +1,44 @@
+#' @name CharnetEx11.2
+#' 
+#' @title Distribuição de Trabalho em um Departamento de Contabilidade
+#' 
+#' @description Estudo para determinar as atividades mais importantes
+#'     dos funcionários do departamento de contabilidade em uma empresa,
+#'     durante 30 dias.
+#'
+#' @format Um \code{data.frame} com 7 colunas e 30 linhas.
+#'
+#' \describe{
+#'
+#' \item{\code{y}}{Número de horas trabalhada por dia.}
+#' 
+#' \item{\code{x1}}{Número de cheques descontados.}
+#' 
+#' \item{\code{x2}}{Número de pagamentos recebidos pelos funcionários.}
+#' 
+#' \item{\code{x3}}{Número de documentos processados e enviados ao banco
+#'     para compensação.}
+#'     
+#' \item{\code{x4}}{Número de ordens de pagamento, certificados e 
+#'     recibos de vendas emitidos pelos funcionários.}
+#'     
+#' \item{\code{x5}}{Número de correspondências processadas e enviadas
+#'     aos clientes.}
+#'     
+#' \item{\code{x6}}{Número de documentos processados e outras atividades.}
+#' 
+#' }
+#'
+#' @keywords TODO
+#'
+#' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino,
+#'     H. (2008). Análise de modelos de regressão linear com aplicações
+#'     (2nd ed., p. 356). SP: Editora Unicamp (Cap. 11, exer. 2, p. 273)
+#'
+#' @examples
+#'
+#' data(CharnetEx11.2)
+#'
+#' plot(CharnetEx11.2)
+#'
+NULL
