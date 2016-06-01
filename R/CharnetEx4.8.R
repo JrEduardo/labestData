824c777a52057e8fbea8fd34f6eda1eee9217223
Eduardo Junior <edujrrib@gmail.com>
2016-06-01 15:37:30 -0300

    Adiciona os arquivo .Rd de documentação

--- R/CharnetEx4.8.R
+++ R/CharnetEx4.8.R
@@ -21,7 +21,7 @@
 #' @source Charnet, R., de Luna Freire, C.A., Charnet, E.M.R. & Bonvino,
 #'     H. (2008). Análise de modelos de regressão linear com aplicações
 #'     (2nd ed., p. 356). SP: Editora Unicamp (Cap. 4, exer. 8, p. 84,
-#'     cap, 6, exer. 9, p. 146.)
+#'     cap. 6, exer. 9, p. 146.)
 #'     
 #'
 #' @examples
