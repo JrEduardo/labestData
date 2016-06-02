edb2e362e3462b5b6dc1b572d00b9e87242303e7
Walmes Zeviani <walmes@ufpr.br>
2016-06-02 16:43:44 -0300

    Corrige e adiciona dado do Faria.

--- R/FariaEg3.2.4.R
+++ R/FariaEg3.2.4.R
@@ -1,32 +1,35 @@
 #' @name FariaEg3.2.4
-#' @title Produção de Amêndoas
-#' @description Produção de amêndoas (kg 10 plantas-1 ano-1) de cacau 
-#'     (5 anos)
-#' @format Um \code{data.frame} com 24 observações e 3 variáveis.
+#' @title Produção de Amêndoas de Clones de Cacau
+#' @description Experimento em delineamento inteiramente casualizado que
+#'     estudou a produção de amendoas de clones de cacau.
+#' @format Um \code{data.frame} com 24 observações e 3 variáveis, em que
+#'
 #' \describe{
-#' 
-#' \item{\code{trat}}{ Os níveis do fator são constituídos por 4 
-#'     tratamentos (A, B, C, e D.). }
-#' 
-#' \item{\code{rept}}{ Os níveis do fator são constituídos por 6 
-#'     repetições. }
-#' 
-#' \item{\code{result}}{ Resultados (kg/ano) dos tratamentos. }
-#' 
+#'
+#' \item{\code{clone}}{Fator categórico que representam os clones de
+#'     cacau.}
+#'
+#' \item{\code{rept}}{Inteiro que indica a repetição de cada clone.}
+#'
+#' \item{\code{result}}{Produção de amendoas, em kg 10 plantas\eqn{{-1}}
+#'     ano\eqn{{-1}}.}
+#'
 #' }
-#' @keywords AV
+#' @keywords DIC
 #' @source Faria, J. C. (2009). Notas de aulas expandidas (10th ed.)
-#'     Ilhéus - BA : UESC. (Tabela 3.2.4 pág 46.)
+#'     Ilhéus, BA: UESC. (Tabela 3.2.4, pág 46)
 #' @examples
-#' 
+#'
 #' library(lattice)
-#' 
-#' 
+#'
 #' data(FariaEg3.2.4)
-#' 
-#' 
-#' xyplot(result~rept|trat, data = FariaEg3.2.4, ylab = "Repetições", 
-#'     xlab = "Tratamento")
-#'     
-#'     
+#' str(FariaEg3.2.4)
+#'
+#' unstack(x = FariaEg3.2.4, form = prod ~ clone)
+#'
+#' xyplot(prod ~ clone, data = FariaEg3.2.4,
+#'        ylab = expression("Produção"~
+#'                              (kg~10~plantas^{-1}~ano^{-1})),
+#'        xlab = "Clones de cacau")
+#'
 NULL
