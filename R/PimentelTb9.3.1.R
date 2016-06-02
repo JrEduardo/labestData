4e8681f8f09500962afa15cbdf88e78b5ea2f38f
Walmes Zeviani <walmes@ufpr.br>
2016-06-02 13:07:22 -0300

    Corrige a adiciona dados do Pimentel Cap 9.

--- R/PimentelTb9.3.1.R
+++ R/PimentelTb9.3.1.R
@@ -1,8 +1,9 @@
 #' @name PimentelTb9.3.1
-#' @title Experimento de Adubaçao de Variedades de Cana-de-açúcar
-#' @description Experimento com 5 variedades de cana em quadrado latino
-#'     de 5 x 5, sendo cada parcela repartida em duas subparcelas, uma
-#'     sem adubo e outra com adubação mineral completa.
+#' @title Experimento de Adubação de Variedades de Cana-de-açúcar
+#' @description Experimento com 5 variedades de cana-de-açúcar, em
+#'     delineamento quadrado latino de 5 x 5, sendo cada parcela
+#'     dividida em duas subparcelas, uma sem adubo e outra com adubação
+#'     mineral completa.
 #' @format Um \code{data.frame} com 50 observações e 5 variáveis, em que
 #'
 #' \describe{
@@ -13,29 +14,33 @@
 #' \item{\code{coluna}}{Fator de 5 níveis qualitativos, usado para
 #'     identificar as colunas do quadrado latino de 5 x 5.}
 #'
-#' \item{\code{varied}}{Fator de 5 níveis qualitativos, que são as
-#'     diferentes variedades de cana-de-açúcar.}
+#' \item{\code{varied}}{Fator de 5 níveis qualitativos que são as
+#'     variedades de cana-de-açúcar.}
 #'
-#' \item{\code{adub}}{Fator de 2 níveis qualitativos, do tipo binário,
-#'     que indica a presença (1) ou ausência (0) de adubação mineral
+#' \item{\code{adub}}{Fator de 2 níveis codificados do tipo binário, que
+#'     indica a presença (1) ou ausência (0) de adubação mineral
 #'     completa.}
 #'
 #' \item{\code{prod}}{Produção de de cana-de-açúcar em kg por
 #'     subparcela.}
 #'
 #' }
-#' @keywords PSS
+#' @keywords PS DQL
 #' @source Pimentel-Gomes, F. (2009). Curso de Estatística
 #'     Experimental (15th ed.). Piracicaba, SP: FEALQ. (Tabela 9.3.1)
 #' @examples
 #'
 #' library(lattice)
 #'
+#' data(PimentelTb9.3.1)
+#' str(PimentelTb9.3.1)
+#'
 #' xyplot(prod ~ varied,
-#'        groups = adub,
+#'        groups = adub, type = c("p", "a"),
 #'        data = PimentelTb9.3.1,
-#'        xlab = "Variedades x Presença de Adubação
-#'        (Rosa: Presente, Azul: Ausente)",
-#'        ylab = "Produção em kg por Subparcela")
+#'        xlab = "Variedades",
+#'        ylab = "Produção (kg/parcela)",
+#'        auto.key = list(title = "Adubação", cex.title = 1.1,
+#'                        columns = 2))
 #'
 NULL
