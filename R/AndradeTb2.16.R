96371c8045b40f0ddf12a43a1e97921b9174af16
Bruno Geronymo <geronymobruno@hotmail.com>
2016-09-01 17:24:48 -0300

    Adiciona Tabela 11 (AndradeTb2.16).

---
+++ R/AndradeTb2.16.R
@@ -0,0 +1,29 @@
+#' @name AndradeTb2.16
+#' @title Avaliação da Formação de Calos
+#' @description Avaliação da formação de calos, mais especificamente, da
+#'     altura de calos em cm, após um período de 30 dias \emph{in vitro}
+#'     para os explantes de procedência do cerrado da espécie
+#'     \emph{Mandevilla velutina}. Obs.: É importante que os calos não
+#'     se desenvolvam, pois quanto menor eles forem, maior será o
+#'     desenvolvimento das plantas.
+#' @format Um \code{vetor} numérico com 37 observações.
+#' @keywords AAS
+#' @source Estatística para as ciências agrárias e biológicas: com
+#'     noções de experimentação / Dalton F. Andrade, Paulo J. Ogliari.
+#'     2. ed. rev. e ampl. - Florianópolis: Ed. da UFSC, 2010. (Tabela
+#'     2.16, pág. 96)
+#' @examples
+#'
+#' library(lattice)
+#'
+#' data(AndradeTb2.16)
+#' str(AndradeTb2.16)
+#'
+#' hist(AndradeTb2.16,
+#'      col = "darkturquoise",
+#'      labels = TRUE,
+#'      xlab = "Altura dos Calos (em cm)",
+#'      ylab = "Densidade",
+#'      xlim = c(0, 1))
+#'
+NULL
