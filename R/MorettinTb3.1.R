c59ef348e0ffd85f3cf159605687c0ea20f15c3b
Eduardo Junior <edujrrib@gmail.com>
2016-09-02 08:28:31 -0300

    Corrige documentação seguindo a indicação da classe ts

--- R/MorettinTb3.1.R
+++ R/MorettinTb3.1.R
@@ -1,27 +1,35 @@
 #' @name MorettinTb3.1
-#' @title Consumo de Energia Elétrica
-#' @description Dados de consumo de energia elétrica no Espírito Santo, 
-#'     de Janeiro de 1977 a Dezembro de 1978, arredondados.
-#' @format Um \code{data.frame} com 24 observações e 2 variáveis, em que
+#' @title Consumo de Energia Elétrica no Espírito Santo
+#' @description Dados sobre o consumo de energia elétrica no Espírito
+#'     Santo, de Janeiro de 1977 a Dezembro de 1978, arredondados.
+#' @format Uma série temporal (classe \code{ts}) com 24 observações
+#'     mensais, de 1977 a 1978.
 #'
-#' \describe{
-#'
-#' \item{t}{Número inteiro que indica o momento no tempo da observação.}
-#'
-#' \item{Zt}{Número decimal que identifica o consumo de energia, sem 
-#'     unidade de medida.}
-#'
-#' }
-#' @keywords TS temporais
+#' @keywords TS
 #' @source Morettin, P. A., Toloi, C. M. C. (2006). Análise de Séries
-#'     Temporais (2nd ed.). São Paulo, SP: Editora Egard Blucher. (Tabela 3.1
-#'     pág. 49)
+#'     Temporais (2nd ed.). São Paulo, SP: Editora Egard
+#'     Blucher. (Tabela 3.1, pág. 49; tabela 3.2, pág. 51; tabela 3.3,
+#'     pág. 54; tabela 3.4 pág. 63)
 #' @examples
 #'
 #' data(MorettinTb3.1)
-#' 
 #' str(MorettinTb3.1)
-#' fivenum(MorettinTb3.1$Zt)
-#' 
-#' plot(MorettinTb3.1)
+#'
+#' library(lattice)
+#'
+#' # Série completa
+#' MorettinTb3.1
+#' summary(MorettinTb3.1)
+#' xyplot(MorettinTb3.1, type = c("o", "g"))
+#'
+#' # Apenas os dados da tabela 3.1 e 3.3
+#' (da <- window(MorettinTb3.1, end = c(1978, 12)))
+#' summary(da)
+#' xyplot(da, type = c("o", "g"))
+#'
+#' # Apenas os dados da tabela 3.2
+#' (da <- window(MorettinTb3.1, start = c(1979, 1)))
+#' summary(da)
+#' xyplot(da, type = c("o", "g"))
+#'
 NULL
--- R/MorettinTb3.6.R
+++ R/MorettinTb3.6.R
@@ -1,60 +1,23 @@
 #' @name MorettinTb3.6
-#' @title Índice de IPI
-#' @description Dados de uma série temporal de índice de produto industrial
-#'     do Brasil (IPI), no período de 1969 a 1980. 
-#' @format Um \code{data.frame} com 12 observações e 13 variáveis, em que
+#' @title Índice Produto Industrial do Brasil
+#' @description Dados sobre a variação mensal do índice de produto
+#'     industrial (IPI) do Brasil no período de 1969 a 1980.
+#' @format Uma série temporal (classe \code{ts}) com 139 observações
+#'     mensais, de 1969 a 1980.
 #'
-#' \describe{
-#'
-#' \item{Ano}{Número inteiro que indica o momento o ano da observação.}
-#'
-#' \item{Jan}{Número decimal que identifica o valor da observação no mês de
-#'     Janeiro do respectivo ano.}
-#'
-#' \item{Fev}{Número decimal que identifica o valor da observação no mês de
-#'     Fevereiro do respectivo ano.}
-#'     
-#' \item{Mar}{Número decimal que identifica o valor da observação no mês de
-#'     Março do respectivo ano.}
-#'     
-#' \item{Abr}{Número decimal que identifica o valor da observação no mês de
-#'     Abril do respectivo ano.}
-#'     
-#' \item{Mai}{Número decimal que identifica o valor da observação no mês de
-#'     Maio do respectivo ano.}
-#'     
-#' \item{Jun}{Número decimal que identifica o valor da observação no mês de
-#'     Junho do respectivo ano.}
-#'     
-#' \item{Jul}{Número decimal que identifica o valor da observação no mês de
-#'     Julho do respectivo ano.}
-#'     
-#' \item{Ago}{Número decimal que identifica o valor da observação no mês de
-#'     Agosto do respectivo ano.}
-#'     
-#' \item{Set}{Número decimal que identifica o valor da observação no mês de
-#'     Setembro do respectivo ano.}
-#'     
-#' \item{Out}{Número decimal que identifica o valor da observação no mês de
-#'     Outubro do respectivo ano.}
-#'     
-#' \item{Nov}{Número decimal que identifica o valor da observação no mês de
-#'     Novembro do respectivo ano.}
-#'     
-#' \item{Dez}{Número decimal que identifica o valor da observação no mês de
-#'     Dezembro do respectivo ano.}
-#' }
-#' @keywords TS temporais
+#' @keywords TS
 #' @source Morettin, P. A., Toloi, C. M. C. (2006). Análise de Séries
-#'     Temporais (2nd ed.). São Paulo, SP: Editora Egard Blucher. (Tabela 3.6
-#'     pág. 68)
+#'     Temporais (2nd ed.). São Paulo, SP: Editora Egard
+#'     Blucher. (Tabela 3.6, pág. 68)
 #' @examples
 #'
 #' data(MorettinTb3.6)
-#' 
 #' str(MorettinTb3.6)
-#' lapply(MorettinTb3.6, 1, FUN=mean, na.rm = T)
-#' 
-#' plot(MorettinTb3.6$Jan, ylab = "Janeiro", xlab = "Valor")
-#' 
+#'
+#' MorettinTb3.6
+#' summary(MorettinTb3.6)
+#'
+#' library(lattice)
+#' xyplot(MorettinTb3.6, type = c("o", "g"))
+#'
 NULL
