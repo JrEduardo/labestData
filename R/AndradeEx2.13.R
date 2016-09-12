33ece6803ab6c4fd0502371bad5d0f3619da9261
Bruno Geronymo <geronymobruno@hotmail.com>
2016-09-12 17:22:03 -0300

    Adiciona Tabela 22 (AndradeEx2.13).

---
+++ R/AndradeEx2.13.R
@@ -0,0 +1,30 @@
+#' @name AndradeEx2.13
+#' @title Estudo dos Ovos de \emph{Biomphalaria Taenagophila}
+#' @description Estudo do comportamento da variável número de ovos
+#'     inviáveis de \emph{Biomphalaria taenagophila} (caramujo) em
+#'     ambiente poluído. Para isso tomou-se uma amostra de 23 caramujos.
+#' @format Um \code{vetor} numérico com 23 observações.
+#' @keywords AAS
+#' @source Estatística para as ciências agrárias e biológicas: com
+#'     noções de experimentação / Dalton F. Andrade, Paulo J. Ogliari.
+#'     2. ed. rev. e ampl. - Florianópolis: Ed. da UFSC, 2010.
+#'     (Capítulo 2, Exercício 13, pág. 109)
+#' @examples
+#'
+#' library(lattice)
+#'
+#' data(AndradeEx2.13)
+#' str(AndradeEx2.13)
+#'
+#' hist(AndradeEx2.13,
+#'      labels = TRUE,
+#'      prob = TRUE,
+#'      main = NULL,
+#'      col = "darkturquoise",
+#'      ylim = c(0, 0.12),
+#'      xlab = "Número de Ovos",
+#'      ylab = "Densidade")
+#'
+#' lines(density(AndradeEx2.13), lwd = 2)
+#'
+NULL
