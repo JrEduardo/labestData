da529de57c6c3e4a6a39ed7d30fdca73cb400351
Bruno Geronymo <geronymobruno@hotmail.com>
2016-09-28 15:22:29 -0300

    Adiciona Tabela 31 (AndradeTb2.42).

---
+++ R/AndradeTb2.42.R
@@ -0,0 +1,30 @@
+#' @name AndradeTb2.42
+#' @title Peso de Carne de Mexilhões
+#' @description Resultados da variável peso de carne, em gramas, de
+#'     mexilhões de dois locais: 1) Sambaqui e 2) Manguezal.
+#' @format Um \code{data.frame} com 30 observações e 2 variáveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{local}}{Fator de 2 níveis qualitativos, que são os locais
+#'     habituais dos mexilhões.}
+#'
+#' \item{\code{peso}}{Peso de carne de mexilhões, em gramas.}
+#'
+#' }
+#' @keywords AAS
+#' @source Andrade, D. F., Ogliari, P. J. (2010). Estatística para as
+#'     ciências agrárias e biológicas com noções de experimentação (2nd
+#'     ed.). Florianópolis, SC. Editora da UFSC. (Tabela 2.42, pág. 146)
+#' @examples
+#'
+#' data(AndradeTb2.42)
+#' str(AndradeTb2.42)
+#'
+#' library(lattice)
+#' boxplot(peso ~ local,
+#'         data = AndradeTb2.42,
+#'         xlab = "Local",
+#'         ylab = "Peso de Carne (em gramas)")
+#'
+NULL
