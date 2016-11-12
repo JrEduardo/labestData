6cc63dab5f2e8def0c5897c271c9640830aa6c37
Eduardo Junior <edujrrib@gmail.com>
2016-11-11 23:10:31 -0200

    Realiza pequenas correções e adiciona Rd

--- R/MorettinTb11.4.R
+++ R/MorettinTb11.4.R
@@ -1,16 +1,12 @@
 #' @name MorettinTb11.4
 #' @title Vazões Históricas do Rio Paranaíba
-#' @description Dados sobre as vazões históricas mensais do 
-#'     rio Paranaíba, na Estação Gamela (Companhia Furnas).
-#'     A tabela é referente ao período de Janeiro de 1955 a Dezembro
-#'     de 1975. 
-#'     
-#' @format Uma série temporal (classe \code{ts}), com 252 observações
-#'     mensais, de janeiro de 1955 a dezembro de 1975.
+#' @description Dados sobre as vazões mensais do rio Paranaíba, na
+#'     Estação Gamela (Companhia Furnas). A série é referente ao período
+#'     de Janeiro de 1955 a Dezembro de 1975.
+#' @format Uma série temporal (classe \code{ts}) com 252 observações
+#'     mensais de Janeiro de 1955 a Dezembro de 1975.
 #' @keywords TS
-#' @source Morettin, P. A., Toloi, C. M. C. (2006). Análise de Séries
-#'     Temporais (2nd ed.). São Paulo, SP: Editora Egard
-#'     Blucher. (Tabela 11.4, Página 300)
+#' @source MORETTIN, TOLOI (2006), Tabela 11.4, pág. 300.
 #' @examples
 #'
 #' data(MorettinTb11.4)
