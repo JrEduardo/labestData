4102dc22c28d5c6098c46beae7f85eb91023791a
Eduardo Junior <edujrrib@gmail.com>
2017-03-14 00:40:57 -0300

    Mantém tags de customização de texto nos títulos

--- site/_build-site.sh
+++ site/_build-site.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-## Skip \emph and \enc don't consider in pkgdown
+## Skip \enc don't consider in pkgdown
 find man/ -type f | xargs perl -i -p -e 's/\\enc\{([áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚüÜçÇ]+)\}\{[A-Za-z]+\}/\1/g'
-find man/ -type f | xargs perl -i -p -e 's/\\emph\{([^\}]+)\}/\1/g'
 
 ## Build
 Rscript -e "pkgdown::init_site()"
